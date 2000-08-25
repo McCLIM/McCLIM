de9be11b73ff0de934a6ece0c990ae21be321259
CVS pserver daemon <cvs>
2000-08-25 16:12:53 +0000

    Modified incorrect copyright notice.

--- sheets.lisp
+++ sheets.lisp
@@ -1,10 +1,10 @@
 ;;; -*- Mode: Lisp; Package: CLIM-INTERNALS -*-
 
-;;;  (c) copyright 1998,1999,2000 by 
-;;;  Michael McDonald (mikemac@mikemac.com), 
-;;;  Robert Strandh (strandh@labri.u-bordeaux.fr), 
-;;;  Iban Hatchondo (hatchond@emi.u-bordeaux.fr),
-;;;  Julien Boninfante (boninfan@emi.u-bordeaux.fr).
+;;;  (c) copyright 1998,1999,2000 by Michael McDonald (mikemac@mikemac.com), 
+;;;  (c) copyright 2000 by 
+;;;           Iban Hatchondo (hatchond@emi.u-bordeaux.fr)
+;;;           Julien Boninfante (boninfan@emi.u-bordeaux.fr)
+;;;           Robert Strandh (strandh@labri.u-bordeaux.fr)
 
 ;;; This library is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU Library General Public
