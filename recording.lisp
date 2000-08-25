e31e0cdb0ffa326f3d9ac7e42cbbb751a5d896f5
CVS pserver daemon <cvs>
2000-08-25 16:10:45 +0000

    Added copyright notice to reflect modifications.

--- recording.lisp
+++ recording.lisp
@@ -1,6 +1,8 @@
 ;;; -*- Mode: Lisp; Package: CLIM-INTERNALS -*-
 
 ;;;  (c) copyright 1998,1999,2000 by Michael McDonald (mikemac@mikemac.com)
+;;;  (c) copyright 2000 by 
+;;;           Robert Strandh (strandh@labri.u-bordeaux.fr)
 
 ;;; This library is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU Library General Public
