a67d12a7c6e767d1d61806eca0209e9cc875ea7d
Arnaud Rouanet <rouanet>
2001-08-01 12:52:01 +0000

    Moved the LETF macro from recording.lisp to utils.lisp

--- recording.lisp
+++ recording.lisp
@@ -32,47 +32,6 @@
 ;;; - When DRAWING-P is NIL, should stream cursor move? 
 ;;; - OUTPUT-RECORD is a protocol class, it should not have any slots/methods.
 
-;;; A useful macro. It should be in some other place.
-(in-package :CLIM-INTERNALS)
-(defun check-letf-form (form)
-  (assert (and (listp form)
-               (= 2 (length form)))))
-
-(shadow 'letf)
-(defmacro letf ((&rest forms) &body body &environment env)
-  "LETF ({(Place Value)}*) Declaration* Form* During evaluation of the
-Forms, SETF the Places to the result of evaluating the Value forms.
-The places are SETF-ed in parallel after all of the Values are
-evaluated."
-  (mapc #'check-letf-form forms)
-  (let* (init-let-form save-old-values-let-form
-         new-values-set-form old-values-set-form
-         update-form)
-    (loop for (place new-value) in forms
-          for (vars vals store-vars writer-form reader-form)
-              = (multiple-value-list (get-setf-expansion place env))
-          for (store-var) = store-vars
-          for old-value-name = (gensym)
-          nconc (mapcar #'list vars vals) into temp-init-let-form
-          collect (list old-value-name reader-form) into temp-save-old-values-let-form
-          nconc (list store-var new-value) into temp-new-values-set-form
-          nconc (list store-var old-value-name) into temp-old-values-set-form
-          collect writer-form into temp-update-form
-          finally (setq init-let-form temp-init-let-form
-                        save-old-values-let-form temp-save-old-values-let-form
-                        new-values-set-form temp-new-values-set-form
-                        old-values-set-form temp-old-values-set-form
-                        update-form (cons 'progn temp-update-form)))
-    `(let* ,init-let-form
-       (let ,save-old-values-let-form
-         (unwind-protect
-             (progn (setq ,@new-values-set-form)
-                    ,update-form
-                    (progn ,@body))
-           (setq ,@old-values-set-form)
-           ,update-form)))))
-;;; ---
-
 (in-package :CLIM-INTERNALS)
 
 (defclass output-record-mixin ()
