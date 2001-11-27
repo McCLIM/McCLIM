6159950c6b2bc62e9625dca51cc85ab307e2a6b2
Mike McDonald <mikemac>
2001-11-27 18:53:08 +0000

    fix for ACL

--- Lisp-Dep/fix-acl.lisp
+++ Lisp-Dep/fix-acl.lisp
@@ -1,6 +1,13 @@
-(defpackage #:clim-mop
-  (:use #:aclmop))
+;;; -*- Mode: Lisp; Package: User -*-
+
+(in-package :common-lisp-user)
+
+(defpackage :clim-mop
+  (:use :clos)
+  (:import-from :common-lisp
+		#:class-name)
+  (:export #:class-name))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (loop for sym being the external-symbols of :aclmop
+  (loop for sym being the external-symbols of :clos
 	do (export sym :clim-mop)))
