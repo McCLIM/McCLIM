f2bb3e2d79727e1b0c0bb6a137aad477be186285
Gilbert Baumann <gbaumann@common-lisp.net>
2002-02-10 01:02:36 +0000

    More random DEFGENERIC forms.

--- decls.lisp
+++ decls.lisp
@@ -39,6 +39,85 @@
 
 (defgeneric transform-region (transformation region))
 
+;; 8.3.4.1 Grafting and Degrafting of Mediums
+
+(defgeneric allocate-medium (port sheet))
+(defgeneric deallocate-medium (port medium))
+(defgeneric make-medium (port sheet))
+(defgeneric engraft-medium (medium port sheet))
+(defgeneric degraft-medium (medium port sheet))
+
+;; 8.4.1 Repaint Protocol Functions
+
+(defgeneric queue-repaint (sheet repaint-event))
+(defgeneric handle-repaint (sheet region))
+(defgeneric repaint-sheet (sheet region))
+
+;; 9 Ports, Grafts, and Mirrored Sheets
+
+;; (defgeneric portp (object))
+;; find-port function
+
+;; 9.4.1 Mirror Functions
+
+(defgeneric sheet-direct-mirror (sheet))
+(defgeneric sheet-mirrored-ancestor (sheet))
+(defgeneric sheet-mirror (sheet))
+(defgeneric realize-mirror (port mirrored-sheet))
+(defgeneric destroy-mirror (port mirrored-sheet))
+(defgeneric raise-mirror (port sheet))
+(defgeneric bury-mirror (port sheet))
+(defgeneric port (object))
+
+;; 9.4.2 Internal Interfaces for Native Coordinates
+
+(defgeneric sheet-native-transformation (sheet))
+(defgeneric sheet-native-region (sheet))
+(defgeneric sheet-device-transformation (sheet))
+(defgeneric sheet-device-region (sheet))
+(defgeneric invalidate-cached-transformations (sheet))
+(defgeneric invalidate-cached-regions (sheet))
+
+;;; Graphics ops
+
+(defgeneric medium-draw-point* (medium x y))
+(defgeneric medium-draw-points* (medium coord-seq))
+(defgeneric medium-draw-line* (medium x1 y1 x2 y2))
+(defgeneric medium-draw-lines* (medium coord-seq))
+(defgeneric medium-draw-polygon* (medium coord-seq closed filled))
+(defgeneric medium-draw-rectangle* (medium left top right bottom filled))
+(defgeneric medium-draw-ellipse* (medium center-x center-y
+				  radius-1-dx radius-1-dy radius-2-dx radius-2-dy
+				  start-angle end-angle filled))
+(defgeneric medium-draw-text* (medium string x y
+			       start end
+			       align-x align-y
+			       toward-x toward-y transform-glyphs))
+
+;;;
+
+(defgeneric medium-foreground (medium))
+(defgeneric medium-background (medium))
+(defgeneric medium-ink (medium))
+(defgeneric medium-transformation (medium))
+(defgeneric medium-clipping-region (medium))
+(defgeneric medium-line-style (medium))
+(defgeneric medium-line-style (medium))
+(defgeneric medium-text-style (medium))
+(defgeneric medium-default-text-style (medium))
+
+(defgeneric (setf medium-foreground) (new-value medium))
+(defgeneric (setf medium-background) (new-value medium))
+(defgeneric (setf medium-ink) (new-value medium))
+(defgeneric (setf medium-transformation) (new-value medium))
+(defgeneric (setf medium-clipping-region) (new-value medium))
+(defgeneric (setf medium-line-style) (new-value medium))
+(defgeneric (setf medium-line-style) (new-value medium))
+(defgeneric (setf medium-text-style) (new-value medium))
+(defgeneric (setf medium-default-text-style) (new-value medium))
+
+(defgeneric medium-sheet (medium))
+
 ;;;
 
 (defmacro with-special-choices ((sheet) &body body)
