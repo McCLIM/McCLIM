75bb779c5c0d7fad8d6e5b41b45846819026a357
Gilbert Baumann <gbaumann@common-lisp.net>
2002-02-21 03:42:44 +0000

    Patch to teach ACL inline functions, though commented out.
    Does anybody know, which versions of ACL are capable/incapable of
    inlining functions, and how to #+-test for it?

--- Lisp-Dep/fix-acl.lisp
+++ Lisp-Dep/fix-acl.lisp
@@ -14,3 +14,38 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (do-external-symbols (sym :clos)
 	(export sym :clim-mop)))
+
+#+NIL
+(progn
+  (eval-when (:compile-toplevel :load-toplevel :execute)
+    (defvar clim-lisp-patch::*inline-functions* nil))
+
+  (defmacro clim-lisp-patch:declaim (&rest args)
+    (dolist (arg args)
+      (cond ((and (consp arg) (eq (car arg) 'inline))
+             (dolist (k (cdr arg))
+               (pushnew k clim-lisp-patch::*inline-functions*)))))
+    `(declaim ,@args) )
+
+  (defmacro clim-lisp-patch:defun (fun args &body body)
+    (cond ((member fun clim-lisp-patch::*inline-functions*)
+           (cond ((and (consp fun) (eq (car fun) 'setf))
+                  (let ((fnam (intern (concatenate 'string
+                                        "(SETF " (symbol-name (cadr fun)) ")")
+                                      (symbol-package (cadr fun)))))
+                    `(progn
+                       (defsetf ,(cadr fun) (&rest ap) (new-value)
+                         (list* ',fnam new-value ap))
+                       (defun ,fnam ,args .,body)
+                       (define-compiler-macro ,fnam (&rest .args.)
+                         (cons '(lambda ,args .,body)
+                               .args.)))))
+                 (t
+                  `(progn
+                     (defun ,fun ,args .,body)
+                     (define-compiler-macro ,fun (&rest .args.)
+                       (cons '(lambda ,args .,body)
+                             .args.))))))
+          (t
+           `(defun ,fun ,args ,@body)))) )
+
