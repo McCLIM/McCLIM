9180d23aeeffd092693ab12f89a29fb5deb4ce3f
Brian Spilsbury <brian>
2002-04-25 10:30:17 +0000

    Push-button look and Slider bounce-back implemented

--- Looks/pixie.lisp
+++ Looks/pixie.lisp
@@ -58,6 +58,21 @@
 (defmethod effective-gadget-input-area-color ((gadget pixie-gadget))
   +white+)
 
+; refactor this into some kind of dragging mixin
+(defclass enter/exit-arms/disarms-unless-dragging-mixin () ())
+
+(defmethod handle-event :before ((pane enter/exit-arms/disarms-unless-dragging-mixin) (event pointer-enter-event))
+  (declare (ignorable event))
+  (with-slots (dragging) pane
+    (unless dragging
+      (arm-gadget pane))))
+
+(defmethod handle-event :after ((pane enter/exit-arms/disarms-unless-dragging-mixin) (event pointer-exit-event))
+  (declare (ignorable event))
+  (with-slots (dragging) pane
+    (unless dragging
+      (disarm-gadget pane))))
+
 ; Slider (refactor into 'thumbed' gadget?)
 
 (defconstant +pixie-slider-pane-thumb-size+  5000.0)
@@ -69,6 +84,8 @@
   ((dragging
     :initform nil)
    (drag-delta
+     :initform 0)
+   (bounce-value
      :initform 0))
   (:default-initargs
     :border-style :inset
@@ -124,10 +141,11 @@
       (disarmed-callback pane (gadget-client pane) (gadget-id pane))))))
 
 (defmethod handle-event ((pane pixie-slider-pane) (event pointer-button-release-event))
-  (with-slots (armed dragging) pane
+  (with-slots (armed dragging value bounce-value) pane
     (when dragging
       (unless (eq dragging :inside)
-        (setf armed nil)
+        (setf armed nil
+              value bounce-value)
         (disarmed-callback pane (gadget-client pane) (gadget-id pane)))
       (setf dragging nil)
       (dispatch-repaint pane (sheet-region pane)))))
@@ -135,13 +153,14 @@
 (defmethod handle-event ((pane pixie-slider-pane) (event pointer-button-press-event))
   (multiple-value-bind (x y) (transform-position (vertical-gadget-orientation-transformation pane)
                                                  (pointer-event-x event) (pointer-event-y event))
-    (with-slots (armed dragging drag-delta) pane
+    (with-slots (armed dragging drag-delta value bounce-value) pane
       (cond
         ((region-contains-position-p (gadget-thumb-region pane) x y)
          ; Thumb
-         (setf dragging   :inside
-               armed      t
-               drag-delta (- y (bounding-rectangle-min-y (gadget-thumb-region pane)))))
+         (setf dragging     :inside
+               armed        t
+               bounce-value value
+               drag-delta   (- y (bounding-rectangle-min-y (gadget-thumb-region pane)))))
         (t
          ; We only care about the thumb
          nil)))))
@@ -570,22 +589,6 @@
 
 (defmethod draw-toggle-button-indicator ((pane pixie-toggle-button-pane) (type (eql :one-of)) value x1 y1 x2 y2)
   (multiple-value-bind (cx cy) (values (/ (+ x1 x2) 2) (/ (+ y1 y2) 2))
-#|
-    (multiple-value-bind (rx ry) (values (/ (- y2 y1) 2) (/ (- x2 x1) 2))
-      (draw-ellipse* pane cx cy 0 ry rx 0
-                     :start-angle (* 1/4 pi)
-                     :end-angle (* 5/4 pi)
-                     :ink *3d-dark-color*)
-      (draw-ellipse* pane cx cy 0 ry rx 0
-                     :start-angle (* 5/4 pi)
-                     :end-angle (* 9/4 pi)
-                     :ink *3d-light-color*)
-      (draw-ellipse* pane cx cy 0 (max 1 (- ry 2)) (max 1 (- rx 2)) 0
-                     :ink (effective-gadget-input-area-color pane))
-      (when value
-        (draw-ellipse* pane cx cy 0 (max 1 (- ry 4)) (max 1 (- rx 4)) 0
-                       :ink (effective-gadget-foreground pane))))
-|#
     (let ((radius (/ (- y2 y1) 2)))
       (draw-circle* pane cx cy radius
                      :start-angle (* 1/4 pi)
@@ -645,18 +648,90 @@
 
 ; Push Button
 
-(defclass pixie-push-button-pane  (pixie-gadget push-button-pane) ())
+(defclass pixie-push-button-pane  (pixie-gadget push-button-pane slider-pane) (
+  (dragging
+    :initform nil)))
 
+; factor out the dragging code into a mixin
+(defmethod handle-event ((pane pixie-push-button-pane) (event pointer-enter-event))
+  (with-slots (armed dragging) pane
+    (cond
+     ((not armed)
+(print "arming")
+      (setf armed t)
+      (armed-callback pane (gadget-client pane) (gadget-id pane)))
+     (dragging
+      (setf dragging :inside))))
+  (dispatch-repaint pane +everywhere+))
+
+(defmethod handle-event ((pane pixie-push-button-pane) (event pointer-exit-event))
+  (with-slots (armed dragging) pane
+    (cond
+     (dragging
+      (setf dragging :outside))
+     (armed
+      (setf armed nil)
+      (disarmed-callback pane (gadget-client pane) (gadget-id pane)))))
+  (dispatch-repaint pane +everywhere+))
+
+(defmethod handle-event ((pane pixie-push-button-pane) (event pointer-button-press-event))
+  (with-slots (pressedp dragging) pane
+    (setf pressedp t
+          dragging :inside)
+    (dispatch-repaint pane +everywhere+)))
+
+(defmethod handle-event ((pane pixie-push-button-pane) (event pointer-button-release-event))
+  (with-slots (armed pressedp dragging) pane
+    (setf pressedp nil)
+    (when (and armed (eq dragging :inside))
+      (activate-callback pane (gadget-client pane) (gadget-id pane))
+      (setf pressedp nil)
+      (dispatch-repaint pane +everywhere+))))
+
+; factor out the box code and reuse as a border mixin
 (defmethod handle-repaint ((pane pixie-push-button-pane) region)
   (declare (ignore region))
-  (with-slots (armed pressedp) pane
-    (multiple-value-bind (x1 y1 x2 y2) (bounding-rectangle* (sheet-region pane))
-      (draw-rectangle* pane x1 y1 x2 y2 :ink (effective-gadget-background pane))
-      (draw-bordered-rectangle* pane x1 y1 x2 y2
-                                :style (if (and pressedp armed) :inset :outset))
-      (multiple-value-bind (x1 y1 x2 y2) (values (+ x1 *3d-border-thickness* (pane-x-spacing pane))
-                                                 (+ y1 *3d-border-thickness* (pane-y-spacing pane))
-                                                 (- x2 *3d-border-thickness* (pane-x-spacing pane))
-                                                 (- y2 *3d-border-thickness* (pane-y-spacing pane)))
-        (draw-label* pane x1 y1 x2 y2
-                     :ink (pane-inking-color pane))))))
+  (with-slots (armed dragging pressedp) pane
+    (with-bounding-rectangle* (x1 y1 x2 y2) (sheet-region pane)
+      (let ((x1 (+ x1 1))
+            (y1 (+ y1 1))
+            (x2 (- x2 1))
+            (y2 (- y2 1)))
+        (let ((x2 (- x2 1))
+              (y2 (- y2 1)))
+          (cond
+           ((or (not pressedp)
+                (eq dragging :outside))
+            (draw-rectangle* pane x1 y1 x2 y2 :ink (effective-gadget-foreground pane))
+            ;; white outline
+            (draw-line* pane x1 y2 x1 y1 :ink +white+)
+            (draw-line* pane x2 y1 x1 y1 :ink +white+)
+            ;; now for the gray inline
+            (let ((x1 (+ x1 2))
+                  (y1 (+ y1 2))
+                  (x2 (- x2 1))
+                  (y2 (- y2 1)))
+              (draw-line* pane x1 y2 (+ x2 1) y2 :ink +gray54+) ; <- not a typo
+              (draw-line* pane x2 y1 x2 (+ y2 1) :ink +gray54+))
+            ;; now for the black outline
+            (draw-line* pane x1 y2 (+ x2 1) y2 :ink +black+)
+            (draw-line* pane x2 y1 x2 (+ y2 1) :ink +black+)
+            (draw-label* pane x1 y1 x2 y2
+                       :ink (pane-inking-color pane)))
+           (pressedp
+            (draw-rectangle* pane x1 y1 x2 y2 :ink (pane-background pane))
+            ;; white outline
+            (draw-line* pane x1 y2 x1 y1 :ink +gray58+)
+            (draw-line* pane x2 y1 x1 y1 :ink +gray58+)
+            ;; now for the black inline
+            (let ((x1 (+ x1 1))
+                  (y1 (+ y1 1))
+                  (x2 (- x2 2))
+                  (y2 (- y2 2)))
+              (draw-line* pane x1 y1 (+ x2 1) y1 :ink +black+)
+              (draw-line* pane x1 y1 x1 (+ y2 1) :ink +black+))
+            ;; now for the black outline
+            (draw-line* pane x1 y2 (+ x2 1) y2 :ink +white+)
+            (draw-line* pane x2 y1 x2 (+ y2 1) :ink +white+)
+            (draw-label* pane x1 y1 x2 y2
+                       :ink (pane-inking-color pane)))))))))
