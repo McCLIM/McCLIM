d3fc7864b3ad6e8d270c13994a92c1d954d54d31
Gilbert Baumann <gbaumann@common-lisp.net>
2002-06-24 15:57:25 +0000

    Fixes <15639.1091.881219.417525@serveur2.labri.fr>.
    Exposure events were lost, since only the very last exposure event of an
    exposure event sequence was taken.
    
    Also the exposure event region is no longer transformed upon reception.

--- Backends/CLX/port.lisp
+++ Backends/CLX/port.lisp
@@ -332,9 +332,9 @@
                       &allow-other-keys)
   ;; XXX :button code -> :button (decode-x-button-code code)
   (declare (ignorable event-slots))
-  (declare (special *clx-port*))
   (let ((sheet (and window
 		    (port-lookup-sheet *clx-port* window))))
+    (declare (special *clx-port*))
     (when sheet
       (case event-key
 	(:key-press
@@ -374,14 +374,22 @@
            ; consolidate motion notifications
 	   (make-instance 'pointer-motion-event :pointer 0 :button code :x x :y y
 			  :sheet sheet :modifier-state state :timestamp time)))
-	((:exposure :display) ; what is a :display event?
-         ;;(when (eq count 0)
-         ;;  ; this should also consolidate the areas, but try this for now
-           (make-instance 'window-repaint-event
-             :sheet sheet
-             :region (make-rectangle* x y (+ x width) (+ y height)))
-           ;;  )
-         )
+        ;;
+	((:exposure)
+         ;; Notes:
+         ;; . Do not compare count with 0 here, last rectangle in an
+         ;;   :exposure event sequence does not cover the whole region. 
+         ;;
+         ;; . Do not transform the event region here, since
+         ;;   WINDOW-EVENT-REGION does it already. And rightfully so. 
+         ;;   (think about changing a sheet's native transformation).
+         ;;--GB
+         ;;
+         ;; this should still consolidate the areas
+         (make-instance 'window-repaint-event
+           :sheet sheet
+           :region (make-rectangle* x y (+ x width) (+ y height))))
+        ;;
 	(:client-message
 	 (when (eq (xlib:atom-name display (aref data 0)) :wm_delete_window)
 	   (destroy-mirror (port sheet) sheet)
