e976dbab979d3b3a227c857309fde4eab9f34e1b
Alexey Dejneka <adejneka>
2002-07-15 13:34:19 +0000

    Output records are replaced with presentations, HANDLE-EVENTs with presentation translators. Added command loop.

--- Examples/clim-fig.lisp
+++ Examples/clim-fig.lisp
@@ -3,6 +3,8 @@
 ;;;  (c) copyright 2001 by
 ;;;           Arnaud Rouanet (rouanet@emi.u-bordeaux.fr)
 ;;;           Lionel Salabartan (salabart@emi.u-bordeaux.fr)
+;;;  (c) copyright 2002 by
+;;;           Alexey Dejneka (adejneka@comail.ru)
 
 ;;; This library is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU Library General Public
@@ -23,22 +25,11 @@
 
 (defclass canvas-pane (application-pane)
   ((first-point-x :initform nil)
-   (first-point-y :initform nil)
-   (selected-object :initform nil)))
+   (first-point-y :initform nil)))
 
 (defun set-status-line (string)
-  (setf (gadget-value (slot-value *application-frame* 'status))
+  (setf (gadget-value (clim-fig-status *application-frame*))
 	string))
-
-#+nil
-(progn
-(defmethod handle-event :around ((pane t) (event t))
-  (let ((*application-frame* (pane-frame pane)))
-    (call-next-method pane event)))
-
-(defmethod dispatch-event ((pane canvas-pane) event)
-  (handle-event pane event))
-)
 
 (defun draw-figure (pane x y x1 y1 &key fastp)
   (with-slots (line-style current-color fill-mode constrict-mode)
@@ -68,16 +59,10 @@
                      :ink current-color
                      :line-style line-style))
         (:arrow
-         (if fastp
-             (draw-arrow* pane x y x1 y1
-                          :ink current-color
-                          :line-style line-style
-                          :to-head t :head-width 20 :head-length 20)
-             (with-new-output-record (pane)
-               (draw-arrow* pane x y x1 y1
-                            :ink current-color
-                            :line-style line-style
-                            :to-head t :head-width 20 :head-length 20))))
+         (draw-arrow* pane x y x1 y1
+                      :ink current-color
+                      :line-style line-style
+                      :to-head t :head-width 20 :head-length 20))
         (:rectangle
          (draw-rectangle* pane x y x1 y1 :filled fill-mode
                           :ink current-color
@@ -87,33 +72,20 @@
                         :filled fill-mode
                         :ink current-color :line-style line-style))))))
 
-(defmethod handle-event ((pane canvas-pane) (event pointer-button-press-event))
-  (when (= (pointer-event-button event) +pointer-left-button+)
-    (handle-draw-object pane event))
-  (when (= (pointer-event-button event) +pointer-right-button+)
-    (with-slots (first-point-x first-point-y selected-object) pane
-      (setq first-point-x (pointer-event-x event)
-            first-point-y (pointer-event-y event))
-      (setf selected-object (search-selected-object (stream-output-history pane)
-                                                    first-point-x
-                                                    first-point-y)))))
-
-(defun search-selected-object (record x y)
-  (map-over-output-records-containing-position
-     #'(lambda (x)
-         (return-from search-selected-object x))
-     record x y)
-  nil)
-
 (defun signum-1 (value)
   (if (zerop value)
       1
       (signum value)))
 
-(defun handle-draw-object (pane event)
-  (let* ((x1 (pointer-event-x event))
-         (y1 (pointer-event-y event))
-         (pixmap-width (round (bounding-rectangle-width (sheet-region pane))))
+(define-presentation-type figure ())
+
+(define-presentation-method highlight-presentation
+    ((type figure) record stream state)
+  (declare (ignore record stream state))
+  nil)
+
+(defun handle-draw-object (pane x1 y1)
+  (let* ((pixmap-width (round (bounding-rectangle-width (sheet-region pane))))
          (pixmap-height (round (bounding-rectangle-height (sheet-region pane))))
          (canvas-pixmap (allocate-pixmap pane pixmap-width pixmap-height)))
     (copy-to-pixmap pane 0 0 pixmap-width pixmap-height canvas-pixmap)
@@ -141,25 +113,22 @@
       (set-status-line " ")
       (copy-from-pixmap canvas-pixmap 0 0 pixmap-width pixmap-height pane 0 0)
       (deallocate-pixmap canvas-pixmap)
-      (draw-figure pane x1 y1 x y)
+      (with-output-as-presentation (pane nil 'figure
+                                         :single-box t)
+        (draw-figure pane x1 y1 x y))
       (setf (clim-fig-redo-list *application-frame*) nil))))
 
-(defmethod handle-event ((pane canvas-pane) (event pointer-button-release-event))
-  (when (= (pointer-event-button event) +pointer-right-button+)
-    (with-slots (first-point-x first-point-y selected-object) pane
-      (when selected-object
-        (multiple-value-bind (old-x old-y) (output-record-position selected-object)
-          (setf (output-record-position selected-object)
-                (values (+ old-x (- (pointer-event-x event) first-point-x))
-                        (+ old-y (- (pointer-event-y event) first-point-y)))))
-        (with-output-recording-options (*standard-output* :record nil)
-          (with-bounding-rectangle* (x1 y1 x2 y2) (sheet-region pane)
-            (draw-rectangle* pane x1 y1 x2 y2 :ink +background-ink+))
-          (replay-output-record (stream-current-output-record *standard-output*)
-                                *standard-output*))
-        (setf selected-object nil
-              first-point-x nil
-              first-point-y nil)))))
+(defun handle-move-object (pane figure first-point-x first-point-y)
+  (tracking-pointer (pane)
+    (:pointer-button-release (&key event x y)
+      (when (= (pointer-event-button event) +pointer-right-button+)
+        (multiple-value-bind (old-x old-y)
+            (output-record-position figure)
+          (setf (output-record-position figure)
+                (values (+ old-x (- x first-point-x))
+                        (+ old-y (- y first-point-y)))))
+        (window-refresh pane)
+        (return-from handle-move-object)))))
 
 (defun clim-fig ()
   (loop for port in climi::*all-ports*
@@ -211,52 +180,6 @@
                                dashes
                                (line-style-dashes line-style))))
 
-(define-command com-exit ()
-  (throw 'exit nil))
-
-(define-command com-undo ()
-  (let* ((output-history (clim-fig-output-record *application-frame*))
-	 (record-count (output-record-count output-history))
-         (record (and (not (zerop record-count))
-		      (elt (output-record-children output-history)
-			   (1- record-count)))))
-    (if record
-        (progn
-          (erase-output-record record *standard-output*)
-          (push record (clim-fig-redo-list *application-frame*)))
-        (beep))))
-
-(define-command com-redo ()
-  (let* ((record (pop (clim-fig-redo-list *application-frame*))))
-    (if record
-        (progn
-          (stream-add-output-record *standard-output* record)
-          (replay record *standard-output* (bounding-rectangle record)))
-        (beep))))
-
-(define-command com-clear ()
-  (setf (clim-fig-redo-list *application-frame*)
-        (append (coerce (output-record-children (clim-fig-output-record
-						 *application-frame*))
-			'list)
-                (clim-fig-redo-list *application-frame*)))
-  (window-clear *standard-output*))
-
-(make-command-table 'file-command-table
-		    :errorp nil
-		    :menu '(("Exit" :command com-exit)))
-
-(make-command-table 'edit-command-table
-		    :errorp nil
-		    :menu '(("Undo" :command com-undo)
-			    ("Redo" :command com-redo)
-                            ("Clear" :command com-clear)))
-
-(make-command-table 'menubar-command-table
-		    :errorp nil
-		    :menu '(("File" :menu file-command-table)
-                            ("Edit" :menu edit-command-table)))
-
 (define-application-frame clim-fig ()
   ((drawing-mode :initform :line :accessor clim-fig-drawing-mode)
    (output-record :accessor clim-fig-output-record)
@@ -266,9 +189,10 @@
    (fill-mode :initform nil :accessor clim-fig-fill-mode)
    (constrict-mode :initform nil :accessor clim-fig-constrict-mode)
    (status :initform nil :accessor clim-fig-status))
+  (:menu-bar menubar-command-table)
   (:panes
-   (canvas (make-pane 'canvas-pane))
-   (menu-bar (climi::make-menu-bar 'menubar-command-table :height 25))
+   (canvas (make-pane 'canvas-pane
+                      :display-time nil))
    (line-width-slider :slider
 		      :label "Line Width"
 		      :value 1
@@ -353,11 +277,12 @@
           :activate-callback #'(lambda (x)
                                  (declare (ignore x))
                                  (com-clear)))
-   (status :text-field :value "CLIM Fig"))
+   (status :text-field :value "CLIM Fig")
+   (command :interactor :height 20
+            :scroll-bar :vertical))
   (:layouts
    (default
      (vertically ()
-       menu-bar
        (horizontally ()
          (vertically (:width 150)
            (tabling (:height 60)
@@ -371,19 +296,84 @@
            ellipse-button rectangle-button)
          (scrolling (:width 600 :height 400) canvas))
        (horizontally (:height 30) clear undo redo)
-       status)))
-;  #+nil
-   (:top-level (clim-fig-frame-top-level)))
+       command
+       status))))
 
-(defmethod clim-fig-frame-top-level ((frame application-frame) &key)
+(define-presentation-to-command-translator add-figure
+    (blank-area com-add-figure clim-fig
+                :gesture :select ; XXX
+                :echo nil
+                :tester ((window) (typep window 'canvas-pane)))
+  (x y)
+  (list x y))
+
+(define-presentation-to-command-translator move-figure
+    (figure com-move-figure clim-fig
+            :gesture :menu ; XXX
+            :echo nil)
+  (presentation x y)
+  (list presentation x y))
+
+(defmethod run-frame-top-level :around ((frame clim-fig) &key &allow-other-keys)
   (let ((*standard-input* (frame-standard-input frame))
 	(*standard-output* (frame-standard-output frame))
 	(*query-io* (frame-query-io frame)))
     (setf (clim-fig-output-record frame)
           (stream-current-output-record *standard-output*)
           (clim-fig-status frame)
-          (find-if #'(lambda (pane) (typep pane 'text-field-pane))
-                   (frame-panes frame)))
-    (catch 'exit
-      (clim-extensions:simple-event-loop))
-    (frame-exit frame)))
+          (find-pane-named frame 'status))
+    (call-next-method)))
+
+(define-clim-fig-command com-exit ()
+  (frame-exit *application-frame*))
+
+(define-clim-fig-command com-undo ()
+  (let* ((output-history (clim-fig-output-record *application-frame*))
+	 (record-count (output-record-count output-history))
+         (record (and (not (zerop record-count))
+		      (elt (output-record-children output-history)
+			   (1- record-count)))))
+    (if record
+        (progn
+          (erase-output-record record *standard-output*)
+          (push record (clim-fig-redo-list *application-frame*)))
+        (beep))))
+
+(define-clim-fig-command com-redo ()
+  (let* ((record (pop (clim-fig-redo-list *application-frame*))))
+    (if record
+        (progn
+          (stream-add-output-record *standard-output* record)
+          (replay record *standard-output* (bounding-rectangle record)))
+        (beep))))
+
+(define-clim-fig-command com-clear ()
+  (setf (clim-fig-redo-list *application-frame*)
+        (append (coerce (output-record-children (clim-fig-output-record
+						 *application-frame*))
+			'list)
+                (clim-fig-redo-list *application-frame*)))
+  (window-clear *standard-output*))
+
+(define-clim-fig-command (com-add-figure :name nil) ((x 'real) (y 'real))
+  (handle-draw-object (find-pane-named *application-frame* 'canvas) x y))
+
+(define-clim-fig-command (com-move-figure :name nil)
+    ((figure 'figure) (x 'real) (y 'real))
+  (handle-move-object (find-pane-named *application-frame* 'canvas)
+                      figure x y))
+
+(make-command-table 'file-command-table
+		    :errorp nil
+		    :menu '(("Exit" :command com-exit)))
+
+(make-command-table 'edit-command-table
+		    :errorp nil
+		    :menu '(("Undo" :command com-undo)
+			    ("Redo" :command com-redo)
+                            ("Clear" :command com-clear)))
+
+(make-command-table 'menubar-command-table
+		    :errorp nil
+		    :menu '(("File" :menu file-command-table)
+                            ("Edit" :menu edit-command-table)))
