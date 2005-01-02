bd89623e07ceb166b4e88d6734c92a32527d50fe
Andy Hefner <ahefner@common-lisp.net>
2005-01-02 05:08:46 +0000

    Package definition for listener.

---
+++ Apps/Listener/package.lisp
@@ -0,0 +1,12 @@
+;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CL-USER; Base: 10 -*-
+(in-package :cl-user)
+
+(defpackage "CLIM-LISTENER"
+  (:use "CLIM" "CLIM-LISP")
+  (:export #:run-listener #:run-listener-process #:dev-commands))
+
+(in-package :clim-listener)
+
+(eval-when (:load-toplevel)
+;  (format t "~&~%!@#%^!@#!@ ... ~A~%~%" *load-truename*)
+  (defparameter *icon-path* (merge-pathnames #P"icons/" *load-truename*)))
