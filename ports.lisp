7ee5b0172e49e04a60789d027982dad23ca2b03c
Christophe Rhodes <crhodes@common-lisp.net>
2006-03-27 10:44:34 +0000

    Whoops.  A missing piece of the Null backend.  Put :null at the end of *server-path-search-order*.

--- ports.lisp
+++ ports.lisp
@@ -25,7 +25,7 @@
 
 (defvar *default-server-path* nil)
 
-(defvar *server-path-search-order* '(:genera :ms-windows :gtk :clx :x11 :opengl :beagle))
+(defvar *server-path-search-order* '(:genera :ms-windows :gtk :clx :x11 :opengl :beagle :null))
 
 (defun find-default-server-path ()
   (loop for port in *server-path-search-order*
