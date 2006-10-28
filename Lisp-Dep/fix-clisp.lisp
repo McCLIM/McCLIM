c8178338f14340b00525b4514701e576727016ff
Troels Henriksen <thenriksen@common-lisp.net>
2006-10-28 15:22:25 +0000

    For CLISP, the :GRAY package needs to be unlocked at read-time, so use ugly calls to `intern' instead of direct symbols.

--- Lisp-Dep/fix-clisp.lisp
+++ Lisp-Dep/fix-clisp.lisp
@@ -6,15 +6,16 @@
      do (export sym :clim-mop)))
 
 ;; CLIM expects INPUT-STREAM-P to be a generic function.
-(unless (typep #'input-stream-p 'generic-function)
-  (setf (fdefinition 'gray::original-input-stream-p) #'input-stream-p)
-  (fmakunbound 'input-stream-p)
-  (defgeneric input-stream-p (stream)
-    (:method ((stream stream)) (gray::original-input-stream-p stream))))
+(ext:without-package-lock ("GRAY" "COMMON-LISP")
+ (unless (typep #'input-stream-p 'generic-function)
+   (setf (fdefinition (intern "ORIGINAL-INPUT-STREAM-P" (find-package :gray))) #'input-stream-p)
+   (fmakunbound 'input-stream-p)
+   (defgeneric input-stream-p (stream)
+     (:method ((stream stream)) (funcall (fdefinition (intern "ORIGINAL-OUTPUT-STREAM-P" (find-package :gray))) stream))))
 
-;; CLIM expects OUTPUT-STREAM-P to be a generic function.
-(unless (typep #'output-stream-p 'generic-function)
-  (setf (fdefinition 'gray::original-output-stream-p) #'output-stream-p)
-  (fmakunbound 'output-stream-p)
-  (defgeneric output-stream-p (stream)
-    (:method ((stream stream)) (gray::original-output-stream-p stream))))
+ ;; CLIM expects OUTPUT-STREAM-P to be a generic function.
+ (unless (typep #'output-stream-p 'generic-function)
+   (setf (fdefinition (intern "ORIGINAL-OUTPUT-STREAM-P" (find-package :gray))) #'output-stream-p)
+   (fmakunbound 'output-stream-p)
+   (defgeneric output-stream-p (stream)
+     (:method ((stream stream)) (funcall (fdefinition (intern "ORIGINAL-OUTPUT-STREAM-P" (find-package :gray))) stream)))))
