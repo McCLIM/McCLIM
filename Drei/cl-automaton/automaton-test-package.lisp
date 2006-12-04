cbc6cc3f8337816ff7015d81d2520294f5ccf567
Troels Henriksen <thenriksen@common-lisp.net>
2006-12-04 07:48:49 +0000

    Removed old RT-based tests.

--- Drei/cl-automaton/automaton-test-package.lisp
+++
@@ -1,9 +0,0 @@
-;;; -*- mode: lisp -*-
-;;; 
-;;; (c) copyright 2005 by Aleksandar Bakic (a_bakic@yahoo.com)
-;;; 
-
-(defpackage #:eqv-hash-user
-  (:use :cl :rtest #:eqv-hash))
-(defpackage #:automaton-user
-  (:use :cl :rtest #:automaton #:eqv-hash))
