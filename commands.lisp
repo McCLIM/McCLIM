654686faf2adb08a7bad48a3c5700186b63784bd
Troels Henriksen <thenriksen@common-lisp.net>
2008-04-20 07:19:10 +0000

    Fixed broken :insert-default t in command parameters.
    It wasn't being passed along in the call to ACCEPT in the acceptor. I
    feel a bit odd about this fix, because I vaguely recall that this used
    to work, at least in Climacs, but the code pretty obviously filtered
    :insert-default keywords away.

--- commands.lisp
+++ commands.lisp
@@ -663,7 +663,7 @@ examine the type of the command menu item to see if it is
 
 (defun accept-form-for-argument (stream arg)
   (let ((accept-keys '(:default :default-type :display-default
-		       :prompt :documentation)))
+		       :prompt :documentation :insert-default)))
     (destructuring-bind (name ptype &rest key-args
 			 &key (mentioned-default nil mentioned-default-p)
 			 &allow-other-keys)
@@ -692,7 +692,7 @@ examine the type of the command menu item to see if it is
 (defun accept-form-for-argument-partial (stream ptype-arg command-arg
 					 original-command-arg )
   (let ((accept-keys '(:default :default-type :display-default
-		       :prompt :documentation)))
+		       :prompt :documentation :insert-default)))
     (destructuring-bind (name ptype &rest key-args)
 	ptype-arg
       (declare (ignore name))
