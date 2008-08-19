f3c1e3dbd7c5a3940bcde521930aa58b1f258f94
Andy Hefner <ahefner@common-lisp.net>
2008-08-19 15:56:50 +0000

    Apparently, when rgb-designs were merged into the core of mcclim, the output recording definitions got left out.

--- recording.lisp
+++ recording.lisp
@@ -1718,6 +1718,33 @@ were added."
        (if-supplied (pattern pattern)
          (eq (slot-value record 'pattern) pattern))))
 
+;;;; RGB images
+
+(def-grecording draw-image-design (() image-design x y) ()
+  (let ((width (image-width (image image-design)))
+        (height (image-height (image image-design)))
+	(transform (medium-transformation medium)))
+    (setf (values x y) (transform-position transform x y))
+    (values x y (+ x width) (+ y height))))
+
+(defmethod* (setf output-record-position) :around
+            (nx ny (record draw-image-design-output-record))
+  (with-standard-rectangle* (:x1 x1 :y1 y1) record
+    (with-slots (x y) record
+      (let ((dx (- nx x1))
+            (dy (- ny y1)))
+        (multiple-value-prog1 (call-next-method)
+          (incf x dx)
+          (incf y dy))))))
+
+(defrecord-predicate draw-image-design-output-record (x y image-design)
+  (and (if-supplied (x coordinate)
+	 (coordinate= (slot-value record 'x) x))
+       (if-supplied (y coordinate)
+	 (coordinate= (slot-value record 'y) y))
+       (if-supplied (image-design rgb-image-design)
+         (eq (slot-value record 'image-design) image-design))))
+
 ;;;; Text
 
 (def-grecording draw-text ((gs-text-style-mixin) string point-x point-y start end
