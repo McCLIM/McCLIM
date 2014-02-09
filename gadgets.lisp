2487d17d5e861eaaf745a7d57b32541bd4e2c6de
Robert Strandh <robert.strandh@gmail.com>
2014-02-09 05:17:33 +0100

    Added DEFGENERIC form for SCROLL-BAR-UP-REGION.

--- gadgets.lisp
+++ gadgets.lisp
@@ -1332,6 +1332,8 @@ and must never be nil."))
 
 (defgeneric scroll-bar-transformation (scroll-bar))
 
+(defgeneric scroll-bar-up-region (scroll-bar))
+
 (defun scroll-bar/update-display (scroll-bar)
   (with-slots (up-state dn-state tb-state tb-y1 tb-y2
                old-up-state old-dn-state old-tb-state old-tb-y1 old-tb-y2
