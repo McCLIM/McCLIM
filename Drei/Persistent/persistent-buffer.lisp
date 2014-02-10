85be61ee4b33a7925b429003db249b4e381ef4cf
Robert Strandh <robert.strandh@gmail.com>
2014-02-10 04:06:55 +0100

    Added copyright and email address.

--- Drei/Persistent/persistent-buffer.lisp
+++ Drei/Persistent/persistent-buffer.lisp
@@ -1,7 +1,7 @@
 ;;; -*- mode: lisp -*-
 ;;; 
 ;;; (c) copyright 2005 by Aleksandar Bakic (a_bakic@yahoo.com)
-;;; 
+;;; (c) copyright 2014 by Robert Strandh (robert.strandh@gmail.com)
 
 ;;; This library is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU Library General Public
