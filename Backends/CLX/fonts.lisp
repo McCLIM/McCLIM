48a8547874ea306447865ad19af14778deb7aed3
Daniel Kochmański <daniel@turtleware.eu>
2016-09-29 14:39:49 +0200

    cosmetic: cleanup whitespace

--- Backends/CLX/fonts.lisp
+++ Backends/CLX/fonts.lisp
@@ -57,14 +57,14 @@
 ;;;; Font listing implementation:
 
 (defclass clx-font-family (clim-extensions:font-family)
-    ((all-faces :initform nil
-		:accessor all-faces
-		:reader clim-extensions:font-family-all-faces)))
+  ((all-faces :initform nil
+              :accessor all-faces
+              :reader clim-extensions:font-family-all-faces)))
 
 (defclass clx-font-face (clim-extensions:font-face)
-    ((all-sizes :initform nil
-		:accessor all-sizes
-		:reader clim-extensions:font-face-all-sizes)))
+  ((all-sizes :initform nil
+              :accessor all-sizes
+              :reader clim-extensions:font-face-all-sizes)))
 
 (defmethod clim-extensions:port-all-font-families :around
     ((port clx-port) &key invalidate-cache)
@@ -75,49 +75,49 @@
 
 (defun split-font-name (name)
   (loop
-      repeat 12
-      for next = (position #\- name :start 0)
-		 :then (position #\- name :start (1+ next))
-      and prev = nil then next
-      while next
-      when prev
-      collect (subseq name (1+ prev) next)))
+     repeat 12
+     for next = (position #\- name :start 0)
+     :then (position #\- name :start (1+ next))
+     and prev = nil then next
+     while next
+     when prev
+     collect (subseq name (1+ prev) next)))
 
 (defun reload-font-table (port)
   (let ((table (make-hash-table :test 'equal)))
     (dolist (font (xlib:list-font-names (clx-port-display port) "*"))
       (destructuring-bind
-	  (&optional foundry family weight slant setwidth style pixelsize
-                     ;;pointsize xresolution yresolution
-                     ;;spacing averagewidth registry encoding
-                     &rest ignore)
-	  (split-font-name font)
-	(declare (ignore setwidth style ignore))
+            (&optional foundry family weight slant setwidth style pixelsize
+                       ;;pointsize xresolution yresolution
+                       ;;spacing averagewidth registry encoding
+                       &rest ignore)
+          (split-font-name font)
+        (declare (ignore setwidth style ignore))
         (setf pixelsize (parse-integer
                          ;; FIXME: Python thinks pixelsize is NIL, resulting
                          ;; in a full WARNING.  Let's COERCE to make it work.
                          (coerce pixelsize 'string) :junk-allowed t))
-	(unless (or (null family)
+        (unless (or (null family)
                     (null pixelsize)
                     (zerop pixelsize))
-	  (let* ((family-name (format nil "~A ~A" foundry family))
-		 (family-instance
-		  (or (gethash family-name table)
-		      (setf (gethash family-name table)
-			    (make-instance 'clx-font-family
-			      :port port
-			      :name family-name))))
-		 (face-name (format nil "~A ~A" weight slant))
-		 (face-instance
-		  (find face-name (all-faces family-instance)
-			:key #'clim-extensions:font-face-name
-			:test #'equal)))
-	    (unless face-instance
-	      (setf face-instance
-		    (make-instance 'clx-font-face
-		      :family family-instance
-		      :name face-name))
-	      (push face-instance (all-faces family-instance)))
+          (let* ((family-name (format nil "~A ~A" foundry family))
+                 (family-instance
+                  (or (gethash family-name table)
+                      (setf (gethash family-name table)
+                            (make-instance 'clx-font-family
+                                           :port port
+                                           :name family-name))))
+                 (face-name (format nil "~A ~A" weight slant))
+                 (face-instance
+                  (find face-name (all-faces family-instance)
+                        :key #'clim-extensions:font-face-name
+                        :test #'equal)))
+            (unless face-instance
+              (setf face-instance
+                    (make-instance 'clx-font-face
+                                   :family family-instance
+                                   :name face-name))
+              (push face-instance (all-faces family-instance)))
             (pushnew pixelsize (all-sizes face-instance))))))
     (sort (loop
              for family being each hash-value in table
@@ -143,4 +143,3 @@
                      (make-unfriendly-name
                       (clim-extensions:font-face-name face))
                      size)))
-
