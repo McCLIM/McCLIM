de0f37414ccb5350b75afdf124c7bcfbf64bb38b
Daniel Kochmański <daniel@turtleware.eu>
2016-10-11 11:34:52 +0200

    cosmetic: header comment

--- Apps/Listener/dev-commands.lisp
+++ Apps/Listener/dev-commands.lisp
@@ -1,25 +1,15 @@
-(in-package :clim-listener)
-
+;;; -*- Mode: Lisp; Syntax: Common-Lisp; Package: CLIM-LISTENER; -*-
+;;;
+;;; Command table and menu definitions
+;;;
 ;;; (C) Copyright 2003,2008 by Andy Hefner (ahefner@gmail.com)
 ;;; (C) Copyright 2004 by Paolo Amoroso (amoroso@mclink.it)
-
-;;; This library is free software; you can redistribute it and/or
-;;; modify it under the terms of the GNU Library General Public
-;;; License as published by the Free Software Foundation; either
-;;; version 2 of the License, or (at your option) any later version.
 ;;;
-;;; This library is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;;; Library General Public License for more details.
+;;; See toplevel file 'Copyright' for the copyright details.
 ;;;
-;;; You should have received a copy of the GNU Library General Public
-;;; License along with this library; if not, write to the 
-;;; Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
-;;; Boston, MA  02111-1307  USA.
 
+(in-package :clim-listener)
 
-;;; Command table and menu definitions
 
 (define-command-table application-commands)
 
