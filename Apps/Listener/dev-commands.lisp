d3b919399d168822b6a0f890cc5028dfc1971e9e
Daniel Kochmanski <daniel@turtleware.eu>
2017-05-07 23:04:45 +0200

    listener: show-file: use alexandria
    command is still broken.

--- Apps/Listener/dev-commands.lisp
+++ Apps/Listener/dev-commands.lisp
@@ -1421,13 +1421,11 @@ if you are interested in fixing this."))
 		   
 
 (defun show-file (pathname)
-  (let ((*standard-output* (open-window-stream :scroll-bars :both
+  (let ((content (alexandria:read-file-into-string pathname))
+	(*standard-output* (open-window-stream :scroll-bars :both
 					       :label (namestring pathname))))
-    (with-open-file (in pathname)
-      (loop for line = (read-line in nil)
-	 while line
-	 do (progn (princ line)
-		   (terpri))))))
+    (princ content)
+    (terpri)))
 
 ;;; Eval
 
