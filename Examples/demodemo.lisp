14b051f5e1e346e6ae6471058e73147578016e33
Cyrus Harmon <cyrus@bobobeach.com>
2017-05-14 19:23:57 -0700

    ignore x1 and x2 in handle-repaint

--- Examples/demodemo.lisp
+++ Examples/demodemo.lisp
@@ -206,6 +206,7 @@
 (defmethod handle-repaint ((pane foo-pane) region)
   (draw-line* pane 50 50 200 50)
   (multiple-value-bind (x1 y1 x2 y2) (bounding-rectangle* region)
+    (declare (ignore x1 x2))
     (let ((k 20))
       (loop for y from (* k (floor (- y1 10) k)) below (+ y2 10) by k do
             (draw-text* pane (format nil "~D" y) 20 y)))))
