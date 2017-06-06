e0567a567eea7cfe8e036f06aea1815566050de5
Cyrus Harmon <cyrus@bobobeach.com>
2017-06-06 00:21:59 -0700

    remove unneeded declare ignore

--- Apps/Listener/listener.lisp
+++ Apps/Listener/listener.lisp
@@ -133,8 +133,7 @@
      :documentation ((object stream)
                      (declare (ignore object))
                      (format stream "Choose package")))
-    (object)
-  (declare (ignore object)))
+    (object))
 
 ;;; Lisp listener command loop
 
