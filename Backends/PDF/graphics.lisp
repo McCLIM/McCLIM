07959969701850d4545c1c15ba6b1f339f32c03b
Cyrus Harmon <cyrus@bobobeach.com>
2017-06-12 08:52:31 -0700

    add medium-draw-rectangle
     * and a broken medium-draw-rectangles*

--- Backends/PDF/graphics.lisp
+++ Backends/PDF/graphics.lisp
@@ -83,6 +83,31 @@
       (t
        (pdf:stroke)))))
 
+(defmethod medium-draw-rectangle* ((medium pdf-medium) x1 y1 x2 y2 filled)
+  (let ((tr (sheet-native-transformation (medium-sheet medium))))
+    (pdf-actualize-graphics-state medium :line-style :color)
+    (with-transformed-position (tr x1 y1)
+      (with-transformed-position (tr x2 y2)
+        (pdf:rectangle x1 y1 (- x2 x1) (- y2 y1))
+        (if filled
+            (pdf:fill-path)
+            (pdf:stroke))))))
+
+;;;
+;;; this is brokem. i don't understand why.
+#+nil
+(defmethod medium-draw-rectangles* ((medium pdf-medium) position-seq filled)
+  (let ((tr (sheet-native-transformation (medium-sheet medium))))
+    (map-repeated-sequence 'nil 4
+                           (lambda (x1 y1 x2 y2)
+                             (with-transformed-position (tr x1 y1)
+                               (with-transformed-position (tr x2 y2)
+                                 (pdf:rectangle x1 y1 (- x2 x1) (- y2 y1))
+                                 (if filled
+                                     (pdf:fill-path)
+                                     (pdf:stroke)))))
+                           position-seq)))
+
 ;;; Graphics state
 
 (defgeneric pdf-set-graphics-state (medium kind))
