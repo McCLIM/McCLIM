0e13fae03aef76a9d32d50a0cfd3b916e69a8a28
Alessandro Serra <gas2serra@gmail.com>
2017-06-14 21:39:32 +0200

    fix import image's symbols

--- Extensions/image/package.lisp
+++ Extensions/image/package.lisp
@@ -1,7 +1,6 @@
 
 (defpackage :mcclim-image
-  (:use #:clim #:clim-lisp)
-
+  (:use #:clim #:clim-lisp #:clim-extensions)
   (:import-from #:clim-internals
                 #:design
                 #:pattern
@@ -35,29 +34,19 @@
 
 ;;;
 ;;; import symbols into climi
-;;;
+;;; 
 
-(cl:import 'mcclim-image:rgb-image
-           (find-package 'clim-internals))
-(cl:import 'mcclim-image:image-width
-           (find-package 'clim-internals))
-(cl:import 'mcclim-image:image-height
-           (find-package 'clim-internals))
-(cl:import 'mcclim-image:image-alpha-p
-           (find-package 'clim-internals))
-(cl:import 'mcclim-image:image-data
-           (find-package 'clim-internals))
-(cl:import 'mcclim-image:rgb-pattern
-           (find-package 'clim-internals))
-(cl:import 'mcclim-image:rgb-image-design
-           (find-package 'clim-internals))
-(cl:import 'mcclim-image:image
-           (find-package 'clim-internals))
-(cl:import 'mcclim-image:make-pattern-from-bitmap-file
-           (find-package 'clim-internals))
-(cl:import 'mcclim-image:xpm-parse-file
-           (find-package 'clim-internals))
-(cl:import 'mcclim-image:medium-draw-image-design*
-           (find-package 'clim-internals))
-(cl:import 'mcclim-image:medium-data
-           (find-package 'clim-internals))
+(in-package #:clim-internals)
+
+(cl:import 'mcclim-image:rgb-image)
+(cl:import 'mcclim-image:image-width)
+(cl:import 'mcclim-image:image-height)
+(cl:import 'mcclim-image:image-alpha-p)
+(cl:import 'mcclim-image:image-data)
+(cl:import 'mcclim-image:rgb-pattern)
+(cl:import 'mcclim-image:rgb-image-design)
+(cl:import 'mcclim-image:image)
+(cl:import 'mcclim-image:make-pattern-from-bitmap-file)
+(cl:import 'mcclim-image:xpm-parse-file)
+(cl:import 'mcclim-image:medium-draw-image-design*)
+(cl:import 'mcclim-image:medium-data)
