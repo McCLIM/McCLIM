5eb06d9780416b6be8d1d0e61b9282183624d627
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2017-08-31 12:34:03 +0200

    examples: Use closer-mop in method-browser

--- Examples/method-browser.lisp
+++ Examples/method-browser.lisp
@@ -78,7 +78,7 @@ specialized on, removing duplicates"
 ;; class specializer for which no prototype instance is available.
 (defun compute-applicable-methods-from-specializers (gf specializers)
   (if (every #'classp specializers)
-      (compute-applicable-methods-using-classes gf specializers)
+      (c2mop:compute-applicable-methods-using-classes gf specializers)
       (let ((instances
              (mapcar (lambda (s)
                        (cond ((classp s)
