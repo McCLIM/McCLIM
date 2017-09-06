6dbc2fad7965e18ade0f19661099b65b5db40684
Cyrus Harmon <cyrus@bobobeach.com>
2017-09-05 23:18:56 -0700

    support gray-alpha images

--- Extensions/bitmap-formats/bitmaps.lisp
+++ Extensions/bitmap-formats/bitmaps.lisp
@@ -20,21 +20,40 @@
          (width (array-dimension img 1))
          (array (make-array (list height width)
                             :element-type '(unsigned-byte 32))))
-    (opticl:do-pixels (y x) img
-      (let (red green blue)
-        (ecase (array-rank img)
-          (3
-           (setq red (aref img y x 0))
-           (setq green (aref img y x 1))
-           (setq blue (aref img y x 2)))
-          (2
-           (let ((v (aref img y x)))
-             (setq red v)
-             (setq green v)
-             (setq blue v))))
-        (setf (aref array y x)
-              (dpb red (byte 8 0)
-                   (dpb green (byte 8 8)
-                        (dpb blue (byte 8 16)
-                             (dpb (- 255 0) (byte 8 24) 0)))))))
+    (etypecase img
+      (opticl:gray-image
+       (opticl:do-pixels (y x) img
+         (let ((v (aref img y x)))
+           (setf (aref array y x)
+                 (dpb v (byte 8 0)
+                      (dpb v (byte 8 8)
+                           (dpb v (byte 8 16)
+                                (dpb (- 255 0) (byte 8 24) 0))))))))
+
+      ;; FIXME! Do the right thing with the alpha channel!
+      (opticl:gray-alpha-image
+       (opticl:do-pixels (y x) img
+         (let ((v (aref img y x 0)))
+           (setf (aref array y x)
+                 (dpb v (byte 8 0)
+                      (dpb v (byte 8 8)
+                           (dpb v (byte 8 16)
+                                (dpb (- 255 0) (byte 8 24) 0))))))))
+
+      (opticl:rgb-image
+       (opticl:do-pixels (y x) img
+         (setf (aref array y x)
+               (dpb (aref img y x 0) (byte 8 0)
+                    (dpb (aref img y x 1) (byte 8 8)
+                         (dpb (aref img y x 2) (byte 8 16)
+                              (dpb (- 255 0) (byte 8 24) 0)))))))
+
+      ;; FIXME! Do the right thing with the alpha channel!
+      (opticl:rgba-image
+       (opticl:do-pixels (y x) img
+         (setf (aref array y x)
+               (dpb (aref img y x 0) (byte 8 0)
+                    (dpb (aref img y x 1) (byte 8 8)
+                         (dpb (aref img y x 2) (byte 8 16)
+                              (dpb (- 255 0) (byte 8 24) 0))))))))
     array))
