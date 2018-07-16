f5103e31d241b038aab4eca08c1c789847a82972
Zhou Qingyang <18245043314@163.com>
2018-07-16 21:21:51 +0800

    add image utilities support mentioned in document

--- Extensions/render/opticl/opticl-image-utilities.lisp
+++ Extensions/render/opticl/opticl-image-utilities.lisp
@@ -11,35 +11,33 @@
        (make-instance ',class
                       :width new-width
                       :height new-height
-                      :pixels (opticl:resize-image (slot-value image 'pixels) new-height new-width)))))
+                      :pixels (opticl:resize-image (slot-value image 'pixels) new-height new-width)))
 
-(defmethod rotate-image* ((image opticl-rgba-image) angle center-x center-y)
-  (let ((pre-shift (opticl:make-affine-transformation :y-shift (- center-y) :x-shift (- center-x)))
-        (rotate (opticl:make-affine-transformation :theta angle))
-        (post-shift (opticl:make-affine-transformation :y-shift center-y :x-shift center-x))
-        (pixels (slot-value image 'pixels)))
-    (make-instance 'opticl-rgba-image
-                   :width (image-width image)
-                   :height (image-height image)
-                   :pixels (opticl:transform-image
-                            (opticl:transform-image
-                             (opticl:transform-image pixels pre-shift)
-                             rotate)
-                            post-shift))))
-
-
-(defmethod flip-image ((image opticl-rgba-image) direction)
-  (make-instance 'opticl-rgba-image
-                 :width (image-width image)
-                 :height (image-height image)
-                 :pixels (case direction
-                           (:vertical
-                            (opticl:vertical-flip-image (slot-value image 'pixels)))
-                           (:horizontal
-                            (opticl:horizontal-flip-image (slot-value image 'pixels)))
-                           (:both
-                            (opticl:horizontal-flip-image
-                             (opticl:vertical-flip-image (slot-value image 'pixels)))))))
+     (defmethod rotate-image* ((image ,class) angle center-x center-y)
+       (let ((pre-shift (opticl:make-affine-transformation :y-shift (- center-y) :x-shift (- center-x)))
+             (rotate (opticl:make-affine-transformation :theta angle))
+             (post-shift (opticl:make-affine-transformation :y-shift center-y :x-shift center-x))
+             (pixels (slot-value image 'pixels)))
+         (make-instance ',class
+                        :width (image-width image)
+                        :height (image-height image)
+                        :pixels (opticl:transform-image
+                                 (opticl:transform-image
+                                  (opticl:transform-image pixels pre-shift)
+                                  rotate)
+                                 post-shift))))
+     (defmethod flip-image ((image ,class) direction)
+       (make-instance ',class
+                      :width (image-width image)
+                      :height (image-height image)
+                      :pixels (case direction
+                                (:vertical
+                                 (opticl:vertical-flip-image (slot-value image 'pixels)))
+                                (:horizontal
+                                 (opticl:horizontal-flip-image (slot-value image 'pixels)))
+                                (:both
+                                 (opticl:horizontal-flip-image
+                                  (opticl:vertical-flip-image (slot-value image 'pixels)))))))))
 
 
 (define-utilities-image opticl-rgba-image)
