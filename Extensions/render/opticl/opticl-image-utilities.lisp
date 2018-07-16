9d17f7833631923808b008e0e86eb85deb7ddcb4
Zhou Qingyang <18245043314@163.com>
2018-07-16 21:11:24 +0800

    add image utilities mentioned in document

---
+++ Extensions/render/opticl/opticl-image-utilities.lisp
@@ -0,0 +1,47 @@
+(in-package :mcclim-render-internals)
+
+
+;;;
+;;;scale
+;;;
+
+(defmacro define-utilities-image (class)
+  `(progn
+     (defmethod scale-image ((image ,class) new-width new-height)
+       (make-instance ',class
+                      :width new-width
+                      :height new-height
+                      :pixels (opticl:resize-image (slot-value image 'pixels) new-height new-width)))))
+
+(defmethod rotate-image* ((image opticl-rgba-image) angle center-x center-y)
+  (let ((pre-shift (opticl:make-affine-transformation :y-shift (- center-y) :x-shift (- center-x)))
+        (rotate (opticl:make-affine-transformation :theta angle))
+        (post-shift (opticl:make-affine-transformation :y-shift center-y :x-shift center-x))
+        (pixels (slot-value image 'pixels)))
+    (make-instance 'opticl-rgba-image
+                   :width (image-width image)
+                   :height (image-height image)
+                   :pixels (opticl:transform-image
+                            (opticl:transform-image
+                             (opticl:transform-image pixels pre-shift)
+                             rotate)
+                            post-shift))))
+
+
+(defmethod flip-image ((image opticl-rgba-image) direction)
+  (make-instance 'opticl-rgba-image
+                 :width (image-width image)
+                 :height (image-height image)
+                 :pixels (case direction
+                           (:vertical
+                            (opticl:vertical-flip-image (slot-value image 'pixels)))
+                           (:horizontal
+                            (opticl:horizontal-flip-image (slot-value image 'pixels)))
+                           (:both
+                            (opticl:horizontal-flip-image
+                             (opticl:vertical-flip-image (slot-value image 'pixels)))))))
+
+
+(define-utilities-image opticl-rgba-image)
+(define-utilities-image opticl-rgb-image)
+(define-utilities-image opticl-gray-image)
