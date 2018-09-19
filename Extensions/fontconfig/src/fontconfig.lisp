e77e9e9842094899cc8120a4660ca6817f5aa8ce
Cyrus Harmon <ch-github@bobobeach.com>
2018-09-19 17:06:27 -0400

    fontconfig constants renamed as +fc-...+ instead of *fc-...*

--- Extensions/fontconfig/src/fontconfig.lisp
+++ Extensions/fontconfig/src/fontconfig.lisp
@@ -112,12 +112,12 @@
     (list (add-list-value-to-pattern pattern key value))))
 
 (defun charset->lisp (charset-native)
-  (cffi:with-foreign-objects ((bitmap 'fc-char32 *fc-charset-map-size*)
+  (cffi:with-foreign-objects ((bitmap 'fc-char32 +fc-charset-map-size+)
                               (next 'fc-char32))
     (let ((result (fc-char-set-first-page charset-native bitmap next)))
       (loop
-        until (eql result *fc-charset-done*)
-        collect (list result (cffi:foreign-array-to-lisp bitmap `(:array fc-char32 ,*fc-charset-map-size*)))
+        until (eql result +fc-charset-done+)
+        collect (list result (cffi:foreign-array-to-lisp bitmap `(:array fc-char32 ,+fc-charset-map-size+)))
         do (setq result (fc-char-set-next-page charset-native bitmap next))))))
 
 (defun str-list->lisp (str-list)
