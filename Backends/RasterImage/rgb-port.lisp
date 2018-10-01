9fedb6702f1452267da25176402df4cbd7c10a12
Cyrus Harmon <cyrus@bobobeach.com>
2018-10-01 09:39:56 -0700

    provide :initform for rgb-image-pixmap
     * without this the various multiple-inheritance of rgb-image-pixmap
       results in an initform of :nil whereas the slot is designed to be a
       region, not (or null region).
    
     * The error only shows up (I think) when running with safety 3, but
       seems like a legitimate error.

--- Backends/RasterImage/rgb-port.lisp
+++ Backends/RasterImage/rgb-port.lisp
@@ -21,7 +21,7 @@
 ;;;
 
 (defclass rgb-image-pixmap (image-pixmap-mixin basic-pane)
-  ())
+  ((region :initform (make-bounding-rectangle 0 0 100 100))))
 
 
 (defmethod port-allocate-pixmap ((port rgb-image-port) sheet width height)
