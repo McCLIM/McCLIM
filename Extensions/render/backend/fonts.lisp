f06415e72413217c280ccce0f51ff62ee44130dd
Daniel Kochmanski <daniel@turtleware.eu>
2018-10-29 15:09:28 +0100

    render backend: unify partially font rendering

--- Extensions/render/backend/fonts.lisp
+++ Extensions/render/backend/fonts.lisp
@@ -5,85 +5,31 @@
 ;;;
 (defstruct (render-glyph-info
              (:include glyph-info)
-             (:constructor render-glyph-info (id width height left right top bottom advance-width advance-height paths opacity-image)))
-  paths
+             (:constructor render-glyph-info (id width height left right top bottom advance-width advance-height opacity-image)))
   opacity-image)
 
 (defclass render-truetype-font (mcclim-truetype::cached-truetype-font)
   ())
 
-(defmethod font-generate-glyph ((font render-truetype-font) code &optional tr)
-  (declare (ignore tr))
-  (multiple-value-bind (paths left top width height dx dy)
-      (glyph-paths font code)
-    (let ((right (+ left width))
-          (bottom (+ top height))
-          (opacity-image (font-generate-opacity-image paths width height left top)))
-      (render-glyph-info 0 dx dy left right top bottom dx dy paths opacity-image))))
+(defmethod font-generate-glyph ((font render-truetype-font) code
+                                &optional (tr +identity-transformation+))
+  (let ((character (code-char (ldb (byte #.(ceiling (log char-code-limit 2)) 0) code)))
+        (next-character (code-char (ldb (byte #.(ceiling (log char-code-limit 2))
+                                              #.(ceiling (log char-code-limit 2)))
+                                        code))))
+    (multiple-value-bind (arr left top width height dx dy udx udy)
+        (glyph-pixarray font character next-character tr)
+      (let ((right (+ left (array-dimension arr 1)))
+            (bottom (- top (array-dimension arr 0))))
+        (render-glyph-info 0 dx dy left right top bottom dx dy arr)))))
 
-(defun font-glyph-info (font character)
-  (with-slots (mcclim-truetype::char->glyph-info) font
-    (ensure-gethash character mcclim-truetype::char->glyph-info
-                    (font-generate-glyph font (char-code character)))))
-
-(defun font-glyph-paths (font character)
-  (render-glyph-info-paths (font-glyph-info font character)))
-
-(defun font-glyph-opacity-image (font character)
-  (render-glyph-info-opacity-image (font-glyph-info font character)))
-
-;;;
-;;; font to opacity mask image
-;;;
-
-(defun font-generate-opacity-image (paths width height dx dy)
-  (let* ((image (make-image (1+ (* 1 width))
-                            (1+ (* 1 height)))))
-    (if (= (* width height) 0)
-        nil
-        (aa-fill-alpha-paths image nil paths (aa:make-state)
-                       (make-translation-transformation
-                        (- dx) dy)
-                       (make-rectangle* 0 0 (* 1 width) (* 1 height))))
-    image))
+(defun font-glyph-opacity-image (font code)
+  (render-glyph-info-opacity-image (font-glyph-info font code)))
 
 ;;;
 ;;; text geometry
 ;;;
-(defmethod climb:font-text-extents ((font render-truetype-font) string
-                                    &key (start 0) (end (length string)) direction)
-  (declare (ignore direction))
-  (let ((width
-         ;; We could work a little harder and eliminate generic arithmetic
-         ;; here. It might shave a few percent off a draw-text benchmark.
-         ;; Rather silly to obsess over the array access considering that.
-         (macrolet ((compute ()
-                      `(loop
-                          for i from start below end
-                          as char = (aref string i)
-                          as code = (char-code char)
-                          sum (max (climb:font-glyph-right font code)
-                                   (climb:font-glyph-width font code)))))
-           (if (climb:font-fixed-width font)
-               (* (climb:font-fixed-width font) (- end start))
-               (typecase string
-                 (simple-string
-                  (locally (declare (type simple-string string))
-                    (compute)))
-                 (string
-                  (locally (declare (type string string))
-                    (compute)))
-                 (t (compute)))))))
-    (let* ((ascent (climb:font-ascent font))
-           (descent (climb:font-descent font))
-           (height (+ ascent descent))
-           (linegap (- (climb:font-leading font) height)))
-      (values
-       ;; bounding box: xmin ymin xmax ymax
-       0 (- ascent) width descent
-       ;; text properties: left top width height
-       0 (- ascent) width height
-       ;; line properties: ascent descent linegap
-       ascent descent linegap
-       ;; cursor motion: cursor-dx cursor-dy
-       width 0))))
+#+ (or)
+(defmethod climb:font-string-glyph-codes ((font render-truetype-font) string
+                                          &key (start 0) (end (length string)))
+  (map 'vector #'char-code (subseq string start end)))
--- Extensions/render/render/prim-text.lisp
+++ Extensions/render/render/prim-text.lisp
@@ -22,24 +22,23 @@
                        :offset (paths:make-point x y))))
 
 
-(defun paths-from-string (font text fn &key (offset (make-point 0 0))
-                                         (scale-x 1.0) (scale-y 1.0)
-                                         (kerning t) (auto-orient nil))
+(defun paths-from-string (font text fn &key
+                                         (offset (make-point 0 0))
+                                         (kerning t))
   "Extract paths from a string."
-  (declare (ignore scale-x scale-y auto-orient))
   (let ((font-loader (zpb-ttf-font-loader (climb:font-face font)))
         (scale (zpb-ttf-font-units->pixels font)))
     (loop
        for previous-char = nil then char
        for char across text
-       for paths = (font-glyph-paths font char)
-       for opacity-image = (font-glyph-opacity-image font char)
-       for dx = (climb:font-glyph-left font char)
-       for dy = (climb:font-glyph-top font char)
+       for code = (char-code char)
+       for opacity-image = (font-glyph-opacity-image font code)
+       for dx = (climb:font-glyph-left font code)
+       for dy = (climb:font-glyph-top font code)
        for previous-width = nil then width
        for width = (max
-                    (climb:font-glyph-right font char)
-                    (climb:font-glyph-width font char))
+                    (climb:font-glyph-right font code)
+                    (climb:font-glyph-width font code))
        do (when previous-char
             (setf offset
                   (paths-ttf::p+ offset
@@ -51,43 +50,6 @@
                                                                                                  font-loader)
                                                                       0))))
                                                    0))))
-         (funcall fn paths opacity-image dx dy
+         (funcall fn opacity-image dx dy
                   (make-translation-transformation (paths:point-x offset)
                                                    (paths:point-y offset))))))
-
-(defun glyph-paths (font char)
-  "Render a character of 'face', returning a 2D (unsigned-byte 8) array
-   suitable as an alpha mask, and dimensions. This function returns five
-   values: alpha mask byte array, x-origin, y-origin (subtracted from
-   position before rendering), horizontal and vertical advances."
-  (climi::with-lock-held (*zpb-font-lock*)
-    (with-slots (units->pixels size ascent descent) font
-      (let* ((units->pixels (zpb-ttf-font-units->pixels font))
-             (size  (climb:font-size font))
-             (ascent (climb:font-ascent font))
-             (descent (climb:font-descent font))
-             (glyph (zpb-ttf:find-glyph char (zpb-ttf-font-loader
-                                              (climb:font-face font))))
-             (left-side-bearing  (* units->pixels (zpb-ttf:left-side-bearing  glyph)))
-             (right-side-bearing (* units->pixels (zpb-ttf:right-side-bearing glyph)))
-             (advance-width (* units->pixels (zpb-ttf:advance-width glyph)))
-             (bounding-box (map 'vector (lambda (x) (float (* x units->pixels)))
-                                (zpb-ttf:bounding-box glyph)))
-             (min-x (elt bounding-box 0))
-             (min-y (elt bounding-box 1))
-             (max-x (elt bounding-box 2))
-             (max-y (elt bounding-box 3))
-             (width  (- (ceiling max-x) (floor min-x)))
-             (height (- (ceiling max-y) (floor min-y)))
-             (paths (paths-ttf:paths-from-glyph glyph
-                                                :offset (paths:make-point 0 0)
-                                                :scale-x units->pixels
-                                                :scale-y (- units->pixels))))
-        (declare (ignore SIZE ASCENT DESCENT LEFT-SIDE-BEARING RIGHT-SIDE-BEARING))
-        (values paths
-                (floor min-x)
-                (ceiling max-y)
-                width
-                height
-                (round advance-width)
-                0)))))
