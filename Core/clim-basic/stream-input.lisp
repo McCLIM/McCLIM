McCLIM/McCLIM

commit 104b3157d871cfb445a9521dc2225caad503dac9

cleanup: convert-to-gesture: remove unnecessary generic function

Core/clim-basic/stream-input.lisp
@@ -241,34 +241,19 @@ keys read."))
       (event-queue-peek buffer)
       (event-queue-read-no-hang buffer)))
 
-
 (defun repush-gesture (gesture buffer)
   (event-queue-prepend buffer gesture))
 
-(defgeneric convert-to-gesture (event))
-
-(defmethod convert-to-gesture ((ev event))
-  nil)
-
-(defmethod convert-to-gesture ((ev character))
-  ev)
-
-(defmethod convert-to-gesture ((ev symbol))
-  ev)
-
-(defmethod convert-to-gesture ((ev key-press-event))
-  (let ((modifiers (event-modifier-state ev))
-	(event ev)
-	(char nil))
-    (when (or (zerop modifiers)
-	      (eql modifiers +shift-key+))
-      (setq char (keyboard-event-character ev)))
-    (if char
-	(char-for-read char)
-	event)))
-
-(defmethod convert-to-gesture ((ev pointer-button-press-event))
-  ev)
+(defun convert-to-gesture (event)
+  (typecase event
+    ((or character symbol pointer-button-event)
+     event)
+    (key-press-event
+     (if-let ((character (keyboard-event-character event))
+              (char-p (member (event-modifier-state event) '(0 +shift-key+))))
+       (char-for-read character)
+       event))
+    (otherwise nil)))
 
 (defmethod stream-read-gesture ((stream standard-extended-input-stream)
 				&key timeout peek-p

commit d0b660cf4db14aaa779ebdc6dc23b598607280ce

Resolve regression in CONVERT-TO-GESTURE
The change 104b3157d871cfb445a9521dc2225caad503dac9 introduced a
problem where modifier keys could not be used together with return.
This fix restores the old behaviour.

Fixes https://github.com/McCLIM/McCLIM/issues/668

Core/clim-basic/stream-input.lisp
@@ -243,10 +243,13 @@ keys read."))
     ((or character symbol pointer-button-event)
      (values gesture (type-of gesture)))
     (key-press-event
-     (if-let ((character (keyboard-event-character gesture))
-              (char-p (member (event-modifier-state gesture) '(0 +shift-key+))))
-       (values (char-for-read character) 'standard-char)
-       (values gesture (type-of gesture))))
+     (let ((modifiers (event-modifier-state gesture))
+           (character (keyboard-event-character gesture)))
+       (if (and (or (zerop modifiers)
+                    (eql modifiers +shift-key+))
+                character)
+           (values (char-for-read character) 'standard-character)
+           (values gesture (type-of gesture)))))
     (otherwise
      nil)))
 