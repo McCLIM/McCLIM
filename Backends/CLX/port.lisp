931e5dae3b2d4cb0d40807a01fe54d1325177a61
Christoph Keßler <ck@plskthx.org>
2019-06-27 20:44:13 +0200

    Fix automagic-clx-server-path: conditionalize on colon-i. (#784)
    The function supposes an occurence of at least one #\: in the DISPLAY
    environment variable. This isn't guaranteed (for example, with XQuartz, DISPLAY
    is set to computer-name.network-name), and in fact the use-localhost
    restart of helpfully-automagic-clx-server-path passes an empty string onwards.

--- Backends/CLX/port.lisp
+++ Backends/CLX/port.lisp
@@ -47,7 +47,7 @@
     (let* (; this code courtesy telent-clx.
            (slash-i (or (position #\/ name) -1))
            (colon-i (position #\: name :start (1+ slash-i)))
-           (decnet-colon-p (eql (elt name (1+ colon-i)) #\:))
+           (decnet-colon-p (and colon-i (eql (elt name (1+ colon-i)) #\:)))
            (host (subseq name (1+ slash-i) colon-i))
            (dot-i (and colon-i (position #\. name :start colon-i)))
            (display (and colon-i
