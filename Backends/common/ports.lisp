0be76ad9e4f696af5f7690e2a6230561bb78734d
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2019-07-16 14:02:14 +0200

    standard-port: remove sheets from MIRRORED-SHEET->CURRENT-POINTER-CURSOR
    Prior to this change, sheets were never removed from the port's
    MIRRORED-SHEET->CURRENT-POINTER-CURSOR table.

--- Backends/common/ports.lisp
+++ Backends/common/ports.lisp
@@ -10,6 +10,9 @@
 (defmethod set-sheet-pointer-cursor :before ((port standard-port) sheet cursor)
   (setf (gethash sheet (slot-value port 'mirrored-sheet->current-pointer-cursor)) cursor))
 
+(defmethod port-unregister-mirror :after ((port standard-port) sheet mirror)
+  (remhash sheet (slot-value port 'mirrored-sheet->current-pointer-cursor)))
+
 (defun stored-object (port selection)
   (check-type port standard-port)
   (check-type selection symbol)
