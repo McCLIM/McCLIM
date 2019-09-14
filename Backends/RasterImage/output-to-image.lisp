652d94846a4f018e2f0ac70a9fa2223f9e7e954f
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2019-09-14 16:45:25 +0200

    rasterimage: use WITH-PORT and other cleanup
    * Introduce UNIT-FACTOR to reduce code duplication.
    
    * Remove unused macro WITH-OUTPUT-TO-RGBA-IMAGE.
    
    * Small cosmetic changes.

--- Backends/RasterImage/output-to-image.lisp
+++ Backends/RasterImage/output-to-image.lisp
@@ -42,28 +42,6 @@
          (invoke-with-output-to-raster-image
           #',cont #',enter-fn #',exit-fn :rgb-image ,format ,@options)))))
 
-(defmacro with-output-to-rgba-image ((stream-var image &rest options)
-                                     &body body)
-  (alexandria:with-gensyms (cont exit-fn enter-fn)
-    (alexandria:once-only (image)
-      `(flet ((,cont (,stream-var)
-                ,@body)
-              (,exit-fn (sheet stream)
-                (declare (ignore stream))
-                (image-mirror-image (sheet-mirror sheet)))
-              (,enter-fn (sheet stream)
-                (declare (ignore stream))
-                (when ,image
-                  (setf (image-mirror-image sheet) ,image))))
-         (declare (dynamic-extent #',cont))
-         (invoke-with-output-to-raster-image
-          #',cont #',enter-fn #',exit-fn :rgb-image :rgb-image ,@options)))))
-
-(defmacro with-output-to-rgba-pattern ((stream-var &rest options)
-                                       &body body)
-  `(with-output-to-rgba-image (,stream-var nil ,@options)
-     ,@body))
-
 (defmacro with-output-to-image ((stream-var image &rest options)
                                 &body body)
   (alexandria:with-gensyms (cont exit-fn enter-fn)
@@ -81,6 +59,11 @@
          (invoke-with-output-to-raster-image
           #',cont #',enter-fn #',exit-fn :rgb-image :rgb-image ,@options)))))
 
+(defmacro with-output-to-rgba-pattern ((stream-var &rest options)
+                                       &body body)
+  `(with-output-to-image (,stream-var nil ,@options)
+     ,@body))
+
 (defmacro with-output-to-image-pattern ((stream-var &rest options)
                                         &body body)
   `(with-output-to-image (,stream-var nil ,@options)
@@ -89,25 +72,23 @@
 (defun invoke-with-output-to-raster-image (continuation enter-fn exit-fn server format
                                            &key (width 1000) (height 1000)
                                                 (border-width 0) (recording-p t))
-  (let ((port (find-port :server-path (list server
-                                            :width width :height height)))
-        (result nil))
-    (let* ((top-level-sheet (make-raster-top-level-sheet port format))
-           (vbox (make-instance 'vbox-pane :port port))
-           (border-pane (make-instance 'climi::border-pane
-                                       :port port
-                                       :border-width border-width)))
+  (with-port (port server :width width :height height)
+    (let ((top-level-sheet (make-raster-top-level-sheet port format))
+          (vbox (make-instance 'vbox-pane :port port))
+          (border-pane (make-instance 'climi::border-pane
+                                      :port port
+                                      :border-width border-width))
+          (stream (make-raster-image-stream port)))
+      (sheet-adopt-child border-pane stream)
+      (sheet-adopt-child vbox border-pane)
       (sheet-adopt-child top-level-sheet vbox)
-      (let ((stream (make-raster-image-stream port)))
-        (clim:sheet-adopt-child border-pane stream)
-        (sheet-adopt-child vbox border-pane)
-        (funcall enter-fn top-level-sheet stream)
-        (realize-mirror port top-level-sheet)
-        (setf (sheet-region top-level-sheet)
-              (clim:make-rectangle* 0 0 width height))
-        (with-output-recording-options (stream :record recording-p :draw t)
-          (funcall continuation stream)
-          (medium-finish-output (sheet-medium stream)))
-        (setf result (funcall exit-fn top-level-sheet stream))))
-    (destroy-port port)
-    result))
+      (funcall enter-fn top-level-sheet stream)
+      (realize-mirror port top-level-sheet)
+      (setf (sheet-region top-level-sheet)
+            (make-rectangle* 0 0 width height))
+      (with-output-recording-options (stream :record recording-p :draw t)
+        (funcall continuation stream)
+        (medium-finish-output (sheet-medium stream)))
+      (values (funcall exit-fn top-level-sheet stream)
+              (when recording-p
+                (stream-output-history stream))))))
