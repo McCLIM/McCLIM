McCLIM/McCLIM

commit b2e1930b8107b166ecffe42d019197eef47e0df7

remove climb:text-style-to-font
The generic CLIMB:TEXT-STYLE-TO-FONT is removed because is a replica of
CLIM:TEXT-STYLE-MAPPING. Each call of CLIMB:TEXT-STYLE-TO-FONT
is substitute with a call of CLIM:TEXT-STYLE-MAPPING.

The :around method of CLIM:TEXT-STYLE-MAPPING for BASIC-PORT save the
mapping for the STANDARD-TEXT-STYLE in a slot of the port.

Backends/CLX/fonts.lisp
@@ -41,8 +41,9 @@
     (when fonts
       (xlib:open-font display (first fonts)))))
 
-(defmethod climb:text-style-to-font ((port clx-port) text-style
-                                     &aux (text-style (climb:parse-text-style* text-style)))
+(defmethod text-style-mapping ((port clx-port) text-style &optional character-set
+                               &aux (text-style (climb:parse-text-style* text-style)))
+  (declare (ignore character-set))
   (labels
       ((find-and-make-xlib-face (display family face size)
          (let* ((family-name (if (stringp family)
@@ -75,9 +76,7 @@
            (setf size (max 2 size))
            (let ((display (clim-clx::clx-port-display port)))
              (find-and-make-xlib-face display family face size)))))
-    (or (text-style-mapping port text-style)
-        (setf (climi::text-style-mapping port text-style)
-              (find-font)))))
+    (find-font)))
 
 
 

Core/clim-basic/medium.lisp
@@ -206,12 +206,16 @@
                               (style2 device-font-text-style))
   (eq style1 style2))
 
-(defmethod text-style-mapping ((port basic-port) text-style
-                               &optional character-set)
+;; the standard-text-style are registered in a port slot.
+(defmethod text-style-mapping :around ((port basic-port) text-style
+                                       &optional character-set
+                                       &aux (text-style (parse-text-style text-style)))
   (declare (ignore character-set))
-  (if (keywordp text-style)
-      (gethash (parse-text-style text-style) (port-text-style-mappings port))
-      (gethash text-style (port-text-style-mappings port))))
+  (if  (typep text-style 'standard-text-style)
+       (alexandria:if-let (font (gethash text-style (port-text-style-mappings port)))
+         font
+         (setf (gethash text-style (port-text-style-mappings port)) (call-next-method)))
+       (call-next-method)))
 
 (defmethod (setf text-style-mapping) (mapping (port basic-port)
                                       text-style

Extensions/fonts/freetype.lisp
@@ -353,7 +353,7 @@ or NIL if the current transformation is the identity transformation."
       (return-from clim:medium-draw-text*))
     (clim-clx::with-clx-graphics () medium
       (unless (eq align-y :baseline)
-        (let* ((font (climb:text-style-to-font (clim:port medium) (clim:medium-text-style medium)))
+        (let* ((font (clim:text-style-mapping (clim:port medium) (clim:medium-text-style medium)))
                (ascent (climb:font-ascent font))
                (descent (climb:font-descent font))
                (text-height (+ ascent descent)))
@@ -387,7 +387,7 @@ or NIL if the current transformation is the identity transformation."
                       (direction :ltr)
                       transformation transform-glyphs
                     &aux
-                      (font (climb:text-style-to-font (clim:port medium) (clim:medium-text-style medium))))
+                      (font (clim:text-style-mapping (clim:port medium) (clim:medium-text-style medium))))
   (unless (or transform-glyphs (clim:translation-transformation-p transformation))
     (multiple-value-setq (x y) (clim:transform-position transformation x y))
     (setq transformation clim:+identity-transformation+))
@@ -405,7 +405,7 @@ or NIL if the current transformation is the identity transformation."
        with curr-x = x
        for (string family style) in blocks
        for new-text-style = (if family (clim:make-text-style family style size) text-style)
-       do (let ((font (climb:text-style-to-font (clim:port medium) new-text-style)))
+       do (let ((font (clim:text-style-mapping (clim:port medium) new-text-style)))
             (%freetype-draw-glyphs font mirror gc curr-x y string
                                    :direction direction
                                    :transformation transformation)
@@ -514,7 +514,7 @@ or NIL if the current transformation is the identity transformation."
                   with sizes = nil
                   for (string family style) in blocks
                   for new-text-style = (if family (clim:make-text-style family style size) text-style)
-                  do (let ((font (climb:text-style-to-font port new-text-style)))
+                  do (let ((font (text-style-mapping port new-text-style)))
                        (setf sizes (multiple-value-list (text-extents font string 0 (length string))))
                        (incf curr-x (car sizes)))
                   finally
@@ -535,7 +535,7 @@ or NIL if the current transformation is the identity transformation."
   (when (= start end)
     (return-from climb:text-bounding-rectangle* (values 0 0 0 0)))
   (let ((text (string string))
-        (font (climb:text-style-to-font (clim:port medium)
+        (font (clim:text-style-mapping (clim:port medium)
                                         (clim:merge-text-styles text-style
                                                                 (clim:medium-merged-text-style medium)))))
     (multiple-value-bind (xmin ymin xmax ymax)
@@ -551,7 +551,7 @@ or NIL if the current transformation is the identity transformation."
     (when (= start end)
       (return-from climb:text-size (values 0 0 0 0 (clim:text-style-ascent text-style medium))))
     (let ((text (string string))
-          (font (climb:text-style-to-font (clim:port medium) text-style)))
+          (font (clim:text-style-mapping (clim:port medium) text-style)))
       (multiple-value-bind (xmin ymin xmax ymax
                             left top width height
                             ascent descent linegap
@@ -621,10 +621,9 @@ or NIL if the current transformation is the identity transformation."
                                        (font-replacement-text-style text-style)
                                        (otherwise nil)))))))
 
-(defmethod climb:text-style-to-font ((port clx-freetype-port) (text-style clim:standard-text-style))
-  (or (clim:text-style-mapping port text-style)
-      (setf (clim:text-style-mapping port text-style)
-            (find-freetype-font port text-style))))
+(defmethod clim:text-style-mapping ((port clx-freetype-port) (text-style clim:standard-text-style) &optional character-set)
+  (declare (ignore character-set))
+  (find-freetype-font port text-style))
 
 ;;;
 ;;;  List fonts
@@ -756,7 +755,7 @@ or NIL if the current transformation is the identity transformation."
              '(nil nil))))))
 
 (defun text-style-contains-char-p (port text-style ch)
-  (let* ((font (climb:text-style-to-font port text-style))
+  (let* ((font (clim:text-style-mapping port text-style))
          (charset (clim-freetype::freetype-font-face/charset (clim-freetype::freetype-font/face font))))
     (mcclim-fontconfig:charset-contains-char-p charset ch)))
 
@@ -779,7 +778,7 @@ or NIL if the current transformation is the identity transformation."
                              (find-best-font-for-fallback-internal port text-style ch)))
 
 (defun find-replacement-fonts (port text-style string)
-  (let* ((default-font (climb:text-style-to-font port text-style))
+  (let* ((default-font (clim:text-style-mapping port text-style))
          (default-charset (clim-freetype::freetype-font-face/charset (clim-freetype::freetype-font/face default-font)))
          (result nil)
          (current-string (make-string-output-stream))

Extensions/render/backend/port.lisp
@@ -24,8 +24,10 @@
 ;;; Fonts
 ;;;
 
-(defmethod climb:text-style-to-font ((port render-port-mixin) (text-style standard-text-style)
-                                     &aux (text-style (climb:parse-text-style* text-style)))
+(defmethod text-style-mapping ((port render-port-mixin) (text-style standard-text-style)
+                               &optional character-set
+                               &aux (text-style (climb:parse-text-style* text-style)))
+  (declare (ignore character-set))
   (labels
       ((find-and-make-truetype-font (family face size)
          (let* ((font-path-maybe-relative
@@ -50,13 +52,12 @@
        (find-font ()
          (multiple-value-call #'find-and-make-truetype-font
            (clim:text-style-components text-style))))
-    (or (text-style-mapping port text-style)
-        (setf (climi::text-style-mapping port text-style)
-              (or (find-truetype-font port text-style)
-                  (invoke-with-truetype-path-restart #'find-font))))))
+    (or (find-truetype-font port text-style)
+                  (invoke-with-truetype-path-restart #'find-font))))
 
-(defmethod climb:text-style-to-font ((port render-port-mixin) (gs-text-style cons))
-  (text-style-to-font port (apply #'make-text-style gs-text-style)))
+(defmethod text-style-mapping ((port render-port-mixin) (gs-text-style cons) &optional character-set)
+  (declare (ignore character-set))
+  (text-style-mapping port (apply #'make-text-style gs-text-style)))
 
 (defmethod clim-internals::text-style-size ((gs-text-style cons))
   (caddr gs-text-style))

Backends/CLX/medium.lisp
@@ -67,7 +67,7 @@
       (let ((old-text-style (medium-text-style medium)))
         (unless (eq text-style old-text-style)
           (setf (xlib:gcontext-font gc)
-                (climb:text-style-to-font (port medium) (medium-text-style medium))))))))
+                (text-style-mapping (port medium) (medium-text-style medium))))))))
 
 ;;; Translate from CLIM styles to CLX styles.
 (defconstant +cap-shape-map+ '((:butt . :butt)
@@ -271,7 +271,7 @@ translated, so they begin at different position than [0,0])."))
       (setf (xlib:gcontext-function gc) boole-1)
       (setf (xlib:gcontext-foreground gc) (X-pixel port ink)
             (xlib:gcontext-background gc) (X-pixel port (medium-background medium)))
-      (let ((fn (climb:text-style-to-font port (medium-text-style medium))))
+      (let ((fn (text-style-mapping port (medium-text-style medium))))
         (when (typep fn 'xlib:font)
           (setf (xlib:gcontext-font gc) fn)))
       (unless (eq last-medium-device-region (medium-device-region medium))
@@ -974,7 +974,7 @@ translated, so they begin at different position than [0,0])."))
     (when gc
       (let ((old-text-style (medium-text-style medium)))
         (unless (eq text-style old-text-style)
-          (let ((fn (text-style-to-font (port medium) (medium-text-style medium))))
+          (let ((fn (text-style-mapping (port medium) (medium-text-style medium))))
             (when (typep fn 'xlib:font)
               (setf (xlib:gcontext-font gc)
                     fn))))))))

Backends/common/fonts.lisp
@@ -212,22 +212,20 @@ of letters specified in a separate kerning-table."))
          (parse-text-style* (apply #'make-text-style style)))
         (t (error "Invalid text style specification ~S." style))))
 
-(defgeneric climb:text-style-to-font (port text-style))
-
 (defmethod text-style-ascent (text-style medium)
-  (let ((font (text-style-to-font (port medium) text-style)))
+  (let ((font (text-style-mapping (port medium) text-style)))
     (climb:font-ascent font)))
 
 (defmethod text-style-descent (text-style medium)
-  (let ((font (text-style-to-font (port medium) text-style)))
+  (let ((font (text-style-mapping (port medium) text-style)))
     (climb:font-descent font)))
 
 (defmethod text-style-height (text-style medium)
-  (let ((font (text-style-to-font (port medium) text-style)))
+  (let ((font (text-style-mapping (port medium) text-style)))
     (+ (climb:font-ascent font) (climb:font-descent font))))
 
 (defmethod text-style-character-width (text-style medium char)
-  (climb:font-character-width (text-style-to-font (port medium) text-style) char))
+  (climb:font-character-width (text-style-mapping (port medium) text-style) char))
 
 (defmethod text-style-width (text-style medium)
   (text-style-character-width text-style medium #\M))
@@ -258,7 +256,7 @@ xmin ymin xmax ymax."))
   (when (= start end)
     (return-from climb:text-bounding-rectangle* (values 0 0 0 0)))
   (let ((text (string string))
-        (font (climb:text-style-to-font (clim:port medium)
+        (font (text-style-mapping (clim:port medium)
                                         (clim:merge-text-styles text-style
                                                                 (clim:medium-merged-text-style medium)))))
     (multiple-value-bind (xmin ymin xmax ymax)
@@ -275,7 +273,7 @@ xmin ymin xmax ymax."))
   (when (= start end)
     (return-from climb:text-size (values 0 0 0 0 (clim:text-style-ascent text-style medium))))
   (let ((text (string string))
-        (font (climb:text-style-to-font (clim:port medium) text-style)))
+        (font (text-style-mapping (clim:port medium) text-style)))
     (multiple-value-bind (xmin ymin xmax ymax
                                left top width height
                                ascent descent linegap

Backends/common/medium.lisp
@@ -10,7 +10,7 @@
   (unless (position #\newline string :start start :end end)
     (return-from clim:medium-draw-text* (call-next-method)))
   (setq string (subseq string start end))
-  (let* ((font (text-style-to-font (port medium) (medium-text-style medium)))
+  (let* ((font (text-style-mapping (port medium) (medium-text-style medium)))
          (y-dx (font-leading font)))
     ;; Single line centering is figured out in the primary method, we just fix
     ;; the X/Y if it will be different for the supplied positioning and then

Backends/CLX/medium-xrender.lisp
@@ -131,7 +131,7 @@
     (return-from text-size (values 0 0 0 0 (text-style-ascent text-style medium))))
   (let* ((text-style (merge-text-styles text-style
                                         (medium-merged-text-style medium)))
-         (font (text-style-to-font (port medium) text-style))
+         (font (text-style-mapping (port medium) text-style))
          (text (subseq (string string) start end))
          (ascent (climb:font-ascent font))
          (line-height (+ ascent (climb:font-descent font)))

commit b6da50cd026587ca5c3ed9d2b714c6ace71aeeab

fixes for the TEXT-STYLE-TO-FONT removal
This fixes minor problems introduced in b2e1930b.

* The TEXT-STYLE-MAPPING can be specialized to STANDARD-TEXT-STYLE
  instead of checking the type in the body. It can also be written
  more concisely using ALEXANDRIA:ENSURE-GETHASH.

* One of the TEXT-STYLE-MAPPING calls in the freetype backend lacked
  the CLIM: package prefix.

* Indentation fixes.

Backends/CLX/fonts.lisp
@@ -57,12 +57,13 @@
                     (open-font display
                                (format nil "-~a-~a-*-*-~d-*-*-*-*-*-~a"
                                        family-name face-name size encoding))))
-;;; xxx: this part is a bit problematic - we either list all fonts
-;;; with any possible encoding (what leads to the situation, when our
-;;; font can't render a simple string "abcd") or we end with only a
-;;; partial list of fonts. since we have mcclim-ttf extension which
-;;; handles unicode characters well, this mechanism of getting fonts
-;;; is deprecated and there is no big harm.
+             ;; XXX: this part is a bit problematic - we either list
+             ;; all fonts with any possible encoding (what leads to
+             ;; the situation, when our font can't render a simple
+             ;; string "abcd") or we end with only a partial list of
+             ;; fonts. since we have mcclim-ttf extension which
+             ;; handles unicode characters well, this mechanism of
+             ;; getting fonts is deprecated and there is no big harm.
              (or (try "iso8859-1")
                  (progn
                    (setf family :sans-serif)

Core/clim-basic/medium.lisp
@@ -206,16 +206,14 @@
                               (style2 device-font-text-style))
   (eq style1 style2))
 
-;; the standard-text-style are registered in a port slot.
-(defmethod text-style-mapping :around ((port basic-port) text-style
+(defmethod text-style-mapping :around ((port basic-port)
+                                       (text-style standard-text-style)
                                        &optional character-set
                                        &aux (text-style (parse-text-style text-style)))
   (declare (ignore character-set))
-  (if  (typep text-style 'standard-text-style)
-       (alexandria:if-let (font (gethash text-style (port-text-style-mappings port)))
-         font
-         (setf (gethash text-style (port-text-style-mappings port)) (call-next-method)))
-       (call-next-method)))
+  ;; Cache `standard-text-style' instances.
+  (ensure-gethash text-style (port-text-style-mappings port)
+                  (call-next-method)))
 
 (defmethod (setf text-style-mapping) (mapping (port basic-port)
                                       text-style

Extensions/fonts/freetype.lisp
@@ -514,7 +514,7 @@ or NIL if the current transformation is the identity transformation."
                   with sizes = nil
                   for (string family style) in blocks
                   for new-text-style = (if family (clim:make-text-style family style size) text-style)
-                  do (let ((font (text-style-mapping port new-text-style)))
+                  do (let ((font (clim:text-style-mapping port new-text-style)))
                        (setf sizes (multiple-value-list (text-extents font string 0 (length string))))
                        (incf curr-x (car sizes)))
                   finally

Extensions/render/backend/port.lisp
@@ -53,7 +53,7 @@
          (multiple-value-call #'find-and-make-truetype-font
            (clim:text-style-components text-style))))
     (or (find-truetype-font port text-style)
-                  (invoke-with-truetype-path-restart #'find-font))))
+        (invoke-with-truetype-path-restart #'find-font))))
 
 (defmethod text-style-mapping ((port render-port-mixin) (gs-text-style cons) &optional character-set)
   (declare (ignore character-set))