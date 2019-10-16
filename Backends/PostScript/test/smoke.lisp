46c5626a0ba689756414f8a2b4bbec2dadc38f08
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2019-10-16 14:46:19 +0200

    postscript: move [E]PS output tests into system

--- Backends/PostScript/test/smoke.lisp
+++ Backends/PostScript/test/smoke.lisp
@@ -26,5 +26,78 @@
     (with-open-file (stream "postscript-test.ps" :direction :output
                                                  :if-does-not-exist :create
                                                  :if-exists :supersede)
-     (clim-postscript:with-output-to-postscript-stream (stream stream)
-       (clim-test-util:print-test-page stream)))))
+      (clim-postscript:with-output-to-postscript-stream (stream stream)
+        (clim-test-util:print-test-page stream)))))
+
+(defun invoke-with-output-file (write-continuation read-continuation filename)
+  (unwind-protect
+       (progn
+         (with-open-file (stream filename :direction :output :if-exists :supersede)
+           (funcall write-continuation stream))
+         (with-open-file (stream filename :direction :input)
+           (funcall read-continuation stream)))
+    (ignore-errors (delete-file filename))))
+
+(defmacro with-output-file ((filename)
+                            ((write-stream) &body write-body)
+                            ((read-stream) &body read-body))
+  `(invoke-with-output-file (lambda (,write-stream) ,@write-body)
+                            (lambda (,read-stream) ,@read-body)
+                            ,filename))
+
+(test ps-output
+  "Test producing a PS document."
+
+  (with-output-file ("postscript-test.ps")
+    ((stream)
+     (clim:with-output-to-postscript-stream (stream stream :device-type :a4)
+       (clim:draw-text* stream "Hello, World!" 20 20)))
+    ((stream)
+     (let ((first-line (read-line stream)))
+       (is (eql 11 (mismatch first-line "%!PS-Adobe-")))
+       (is (eq nil (position #\Space first-line)))
+       (do ((line (read-line stream) (read-line stream)))
+           ((not (eql (mismatch line "%%") 2))
+            (error "Failed to find bounding box"))
+         (when (eql (mismatch line "%%BoundingBox: ") 15)
+           (with-input-from-string (string line :start 15)
+             (let ((llx (read string))
+                   (lly (read string))
+                   (urx (read string))
+                   (ury (read string)))
+               (is-true (numberp llx))
+               (is-true (numberp lly))
+               (is-true (numberp urx))
+               (is-true (numberp ury))
+               (return t)))))))))
+
+(test eps-output
+  "Test producing an EPS document."
+
+  (with-output-file ("postscript-test.eps")
+    ((stream)
+     (clim:with-output-to-postscript-stream (stream stream :device-type :eps)
+       (clim:draw-rectangle* stream 1 1 19 21)))
+    ((stream)
+     (let ((first-line (read-line stream)))
+       (is (eql 11 (mismatch first-line "%!PS-Adobe-")))
+       (is-true (search "EPSF" first-line))
+       (do ((line (read-line stream) (read-line stream)))
+           ((not (eql (mismatch line "%%") 2))
+            (error "Failed to find bounding box"))
+         (when (eql (mismatch line "%%BoundingBox: ") 15)
+           (with-input-from-string (string line :start 15)
+             (let ((llx (read string))
+                   (lly (read string))
+                   (urx (read string))
+                   (ury (read string)))
+               (is-true (numberp llx))
+               (is-true (numberp lly))
+               (is-true (numberp urx))
+               (is-true (numberp ury))
+               ;; our EPS files have lower bounds of 0.
+               (is (= 0 llx))
+               (is (= 0 lly))
+               (is (>= 20 (- urx llx) 18))
+               (is (>= 22 (- ury lly) 20))
+               (return t)))))))))
