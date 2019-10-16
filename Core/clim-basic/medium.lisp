McCLIM/McCLIM

commit b6da50cd026587ca5c3ed9d2b714c6ace71aeeab

fixes for the TEXT-STYLE-TO-FONT removal
This fixes minor problems introduced in b2e1930b.

* The TEXT-STYLE-MAPPING can be specialized to STANDARD-TEXT-STYLE
  instead of checking the type in the body. It can also be written
  more concisely using ALEXANDRIA:ENSURE-GETHASH.

* One of the TEXT-STYLE-MAPPING calls in the freetype backend lacked
  the CLIM: package prefix.

* Indentation fixes.

Core/clim-basic/medium.lisp
@@ -206,16 +206,14 @@
                               (style2 device-font-text-style))
   (eq style1 style2))
 
-;; the standard-text-style are registered in a port slot.
-(defmethod text-style-mapping :around ((port basic-port) text-style
+(defmethod text-style-mapping :around ((port basic-port)
+                                       (text-style standard-text-style)
                                        &optional character-set
                                        &aux (text-style (parse-text-style text-style)))
   (declare (ignore character-set))
-  (if  (typep text-style 'standard-text-style)
-       (alexandria:if-let (font (gethash text-style (port-text-style-mappings port)))
-         font
-         (setf (gethash text-style (port-text-style-mappings port)) (call-next-method)))
-       (call-next-method)))
+  ;; Cache `standard-text-style' instances.
+  (ensure-gethash text-style (port-text-style-mappings port)
+                  (call-next-method)))
 
 (defmethod (setf text-style-mapping) (mapping (port basic-port)
                                       text-style

Backends/CLX/fonts.lisp
@@ -57,12 +57,13 @@
                     (open-font display
                                (format nil "-~a-~a-*-*-~d-*-*-*-*-*-~a"
                                        family-name face-name size encoding))))
-;;; xxx: this part is a bit problematic - we either list all fonts
-;;; with any possible encoding (what leads to the situation, when our
-;;; font can't render a simple string "abcd") or we end with only a
-;;; partial list of fonts. since we have mcclim-ttf extension which
-;;; handles unicode characters well, this mechanism of getting fonts
-;;; is deprecated and there is no big harm.
+             ;; XXX: this part is a bit problematic - we either list
+             ;; all fonts with any possible encoding (what leads to
+             ;; the situation, when our font can't render a simple
+             ;; string "abcd") or we end with only a partial list of
+             ;; fonts. since we have mcclim-ttf extension which
+             ;; handles unicode characters well, this mechanism of
+             ;; getting fonts is deprecated and there is no big harm.
              (or (try "iso8859-1")
                  (progn
                    (setf family :sans-serif)

Extensions/fonts/freetype.lisp
@@ -514,7 +514,7 @@ or NIL if the current transformation is the identity transformation."
                   with sizes = nil
                   for (string family style) in blocks
                   for new-text-style = (if family (clim:make-text-style family style size) text-style)
-                  do (let ((font (text-style-mapping port new-text-style)))
+                  do (let ((font (clim:text-style-mapping port new-text-style)))
                        (setf sizes (multiple-value-list (text-extents font string 0 (length string))))
                        (incf curr-x (car sizes)))
                   finally

Extensions/render/backend/port.lisp
@@ -53,7 +53,7 @@
          (multiple-value-call #'find-and-make-truetype-font
            (clim:text-style-components text-style))))
     (or (find-truetype-font port text-style)
-                  (invoke-with-truetype-path-restart #'find-font))))
+        (invoke-with-truetype-path-restart #'find-font))))
 
 (defmethod text-style-mapping ((port render-port-mixin) (gs-text-style cons) &optional character-set)
   (declare (ignore character-set))

commit f7c8f1777c220ddea870624d07f39582830a9422

medium: fix TEXT-STYLE-MAPPING cache again
b6da50cd0 introduced a regression by making the decision whether to
cache the font association before parsing the text style.

This change restores the behavior introduced in b2e1930b8.

Core/clim-basic/medium.lisp
@@ -206,14 +206,16 @@
                               (style2 device-font-text-style))
   (eq style1 style2))
 
-(defmethod text-style-mapping :around ((port basic-port)
-                                       (text-style standard-text-style)
+(defmethod text-style-mapping :around ((port basic-port) text-style
                                        &optional character-set
                                        &aux (text-style (parse-text-style text-style)))
   (declare (ignore character-set))
-  ;; Cache `standard-text-style' instances.
-  (ensure-gethash text-style (port-text-style-mappings port)
-                  (call-next-method)))
+  ;; If the /parsed/ TEXT-STYLE is a `standard-text-style', cache the
+  ;; font association.
+  (if (typep text-style 'standard-text-style)
+      (ensure-gethash text-style (port-text-style-mappings port)
+                      (call-next-method))
+      (call-next-method)))
 
 (defmethod (setf text-style-mapping) (mapping (port basic-port)
                                       text-style