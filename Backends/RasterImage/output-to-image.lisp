McCLIM/McCLIM

commit d59e3120cf2c90c21fec4ed0fcc69a7d47e73f2a

rasterimage: INVOKE-WITH-OUTPUT-TO-RASTER-IMAGE can compute dimensions
When the value :COMPUTE is supplied for :WIDTH, :HEIGHT or both, try
to figure out good dimensions for the image before rendering it.

Backends/RasterImage/output-to-image.lisp
@@ -70,9 +70,9 @@
      ,@body))
 
 (defun invoke-with-output-to-raster-image (continuation enter-fn exit-fn server format
-                                           &key (width 1000) (height 1000)
+                                           &key (width :compute) (height :compute)
                                                 (border-width 0) (recording-p t))
-  (with-port (port server :width width :height height)
+  (with-port (port server :width 1 :height 1)
     (let ((top-level-sheet (make-raster-top-level-sheet port format))
           (vbox (make-instance 'vbox-pane :port port))
           (border-pane (make-instance 'climi::border-pane
@@ -82,10 +82,40 @@
       (sheet-adopt-child border-pane stream)
       (sheet-adopt-child vbox border-pane)
       (sheet-adopt-child top-level-sheet vbox)
-      (funcall enter-fn top-level-sheet stream)
       (realize-mirror port top-level-sheet)
-      (setf (sheet-region top-level-sheet)
-            (make-rectangle* 0 0 width height))
+      (funcall enter-fn top-level-sheet stream)
+
+      ;; When WIDTH or HEIGHT is :COMPUTE, render into an output
+      ;; record and use its dimensions to change the space
+      ;; requirements of STREAM. When WIDTH is :COMPUTE, do this two
+      ;; times: one time to determine the required width and a second
+      ;; time to determine the resulting height when using the
+      ;; computed width.
+      (if (or (eq width :compute) (eq height :compute))
+          (flet ((try ()
+                   (let ((record (with-output-to-output-record (stream)
+                                   (funcall continuation stream))))
+                     ;; FIXME Enlarging the space requirements a bits
+                     ;; is needed to prevent things from getting
+                     ;; clipped.
+                     (change-space-requirements
+                      stream
+                      :width (if (eq width :compute)
+                                 (+ (bounding-rectangle-width record) 2)
+                                 width)
+                      :height (if (eq height :compute)
+                                  (+ (bounding-rectangle-height record) 2)
+                                  height)))))
+            ;; Ensure STREAM's preferred width is set to something
+            ;; reasonable, then call CONTINUATION and update STREAM's
+            ;; space requirements.
+            (change-space-requirements
+             stream :width (if (eq width :compute) 1000 width))
+            (try)
+            (when (eq width :compute)
+              (try)))
+          (change-space-requirements stream))
+
       (with-output-recording-options (stream :record recording-p :draw t)
         (funcall continuation stream)
         (medium-finish-output (sheet-medium stream)))

Backends/RasterImage/test/output-to-image.lisp
@@ -15,18 +15,23 @@
 ;;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;;; Boston, MA  02111-1307  USA.
 
-(cl:defpackage #:mcclim-raster-image.test
-  (:use
-   #:cl
-   #:alexandria
-   #:fiveam)
-
-  (:export
-   #:run-tests))
-
 (cl:in-package #:mcclim-raster-image.test)
 
-(def-suite* :mcclim-raster-image)
+(in-suite :mcclim-raster-image)
 
-(defun run-tests ()
-  (run! :mcclim-raster-image))
+(test with-output-to-raster-image.smoke
+  "Smoke test for the WITH-OUTPUT-TO-RASTER-IMAGE macro."
+
+  (flet ((do-it (width height border-width)
+           (let* ((name (format nil "with-output-to-raster-image-~A-~A-~A"
+                                width height border-width))
+                  (pathname (make-pathname :name name :type "png")))
+             (mcclim-raster-image:with-output-to-raster-image-file
+                 (stream pathname :width width
+                                  :height height
+                                  :border-width border-width)
+               (let ((*standard-output* stream))
+                 (room))))))
+    (map-product (lambda (&rest args)
+                   (finishes (apply #'do-it args)))
+                 '(60 :compute) '(60 :compute) '(0 20))))

Backends/RasterImage/mcclim-raster-image.asd
@@ -20,6 +20,7 @@
   :components ((:module "test"
                 :serial t
                 :components ((:file "package")
-                             (:file "smoke"))))
+                             (:file "smoke")
+                             (:file "output-to-image"))))
   :perform (test-op (operation component)
              (uiop:symbol-call '#:mcclim-raster-image.test '#:run-tests)))

commit 4740e9c29687fd4c31eabfc4052d8c000796d8ff

rasterimage: respect numerically specified output width and height
Not using the WIDTH and HEIGHT arguments unless the values are
:COMPUTE was a regression in d59e3120c.

Backends/RasterImage/output-to-image.lisp
@@ -95,7 +95,7 @@
           (flet ((try ()
                    (let ((record (with-output-to-output-record (stream)
                                    (funcall continuation stream))))
-                     ;; FIXME Enlarging the space requirements a bits
+                     ;; FIXME Enlarging the space requirements a bit
                      ;; is needed to prevent things from getting
                      ;; clipped.
                      (change-space-requirements
@@ -114,7 +114,7 @@
             (try)
             (when (eq width :compute)
               (try)))
-          (change-space-requirements stream))
+          (change-space-requirements stream :width width :height height))
 
       (with-output-recording-options (stream :record recording-p :draw t)
         (funcall continuation stream)