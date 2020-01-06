f5d1f6a62c804c4465be52d60ce59ad1818e1985
Daniel Kochmański <daniel@turtleware.eu>
2020-01-06 20:25:22 +0100

    make-pane-1: add a new utility to map abstract pane type to class
    Until now each backend had their own ad-hoc method. Introduce a
    function protocol which unifies this behavior. New function is called
    find-concrete-pane-class and is exported from CLIMB package.
    
    Also add default make-pane-1 method.

--- Backends/CLX-fb/frame-manager.lisp
+++ Backends/CLX-fb/frame-manager.lisp
@@ -9,16 +9,16 @@
 ;;; if the pane is a subclass of basic-pane and it is not mirrored we create a new class.
 (defun maybe-mirroring (fm concrete-pane-class)
   (when (and (not (subtypep concrete-pane-class 'mirrored-sheet-mixin))
-	     (funcall (clim-clx::mirroring-p fm) concrete-pane-class))
+             (funcall (clim-clx::mirroring-p fm) concrete-pane-class))
     (let* ((concrete-pane-class-symbol (if (typep concrete-pane-class 'class)
-                                          (class-name concrete-pane-class)
-                                          concrete-pane-class)))
+                                           (class-name concrete-pane-class)
+                                           concrete-pane-class)))
       (multiple-value-bind (class-symbol foundp)
           (alexandria:ensure-symbol
            (alexandria:symbolicate (clim-clx::class-gensym fm) "-"
                                    (symbol-name concrete-pane-class-symbol))
            :clim-clx-fb)
-	(unless foundp
+        (unless foundp
           (let ((superclasses (if (subtypep concrete-pane-class 'sheet-with-medium-mixin)
                                   (list 'clx-fb-mirrored-sheet-mixin
                                         'climi::always-repaint-background-mixin
@@ -38,9 +38,11 @@
   concrete-pane-class)
 
 (defmethod make-pane-1 ((fm clx-fb-frame-manager) (frame application-frame) type &rest args)
+  ;; This backend doesn't have any specialized pane implementations
+  ;; but depending on circumstances it may add optional mirroring to
+  ;; the class. Such automatically defined concrete class has the same
+  ;; name but with a gensym prefix and symbol in the backend package.
   (apply #'make-instance
-	 (maybe-mirroring fm (clim-clx::find-concrete-pane-class type))
-	 :frame frame
-	 :manager fm
-	 :port (port frame)
-	 args))
+         (maybe-mirroring fm (find-concrete-pane-class type))
+         :frame frame :manager fm :port (port frame)
+         args))
