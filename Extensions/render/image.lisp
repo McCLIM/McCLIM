5d629947ad5904138a46bd089f86eefccce1b114
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2020-02-09 17:12:42 +0100

    render: basic optimizations (proper version is in a different branch)

--- Extensions/render/image.lisp
+++ Extensions/render/image.lisp
@@ -129,15 +129,149 @@
     (declare (type (simple-array (unsigned-byte 32) 2) src-array))
     (make-instance 'climi::%rgba-pattern :array (alexandria:copy-array src-array))))
 
+(deftype image-index () 'alexandria:array-index)
+(deftype argb-pixel () '(unsigned-byte 32))
+(deftype argb-pixel-array () '(simple-array argb-pixel 2))
+(deftype stencil-array () '(simple-array bit 2))
 (defun fill-image (image design &key (x 0) (y 0)
-                                  (width (pattern-width image))
-                                  (height (pattern-height image))
-                                  stencil (stencil-dx 0) (stencil-dy 0)
-                                  clip-region
-                   &aux
-                     (dst-array (climi::pattern-array image))
-                     (x2 (+ x width -1))
-                     (y2 (+ y height -1)))
+                                     (width (pattern-width image))
+                                     (height (pattern-height image))
+                                     stencil (stencil-dx 0) (stencil-dy 0)
+                                     clip-region)
+  "Blends DESIGN onto IMAGE with STENCIL and a CLIP-REGION."
+  (declare (optimize (speed 3))
+           (type image-index x y width height)
+           (type (signed-byte 33) stencil-dx stencil-dy))
+  ;; Disregard CLIP-REGION if x,y,width,height is entirely contained.
+  (when clip-region
+    (let ((region (make-rectangle* x y (+ x width) (+ y height))))
+      (cond ((region-contains-region-p clip-region region) ; TODO we create almost this rectangle as our return value
+             (setf clip-region nil))
+            ((bounding-rectangle-p clip-region)
+             (with-bounding-rectangle* (x1 y1 x2 y2)
+                 (region-intersection clip-region region)
+               (setf x (floor x1) y (floor y1) width (ceiling (- x2 x1)) height (ceiling (- y2 y1))
+                     clip-region nil))))))
+  (let ((dst-array (climi::pattern-array image))
+        (stencil-array (and stencil (climi::pattern-array stencil)))
+        (x2 (+ x width -1))
+        (y2 (+ y height -1))
+        old-alpha alpha
+        old-ink ink (ink-rgba 0)
+        (static-ink-p t #+TODO (static-ink-p design))
+        mode
+        source-rgba source-r source-g source-b source-a)
+    (declare (type argb-pixel-array dst-array)
+             (type argb-pixel       ink-rgba))
+    (flet ((update-alpha (x y)
+             ;; Set ALPHA according to STENCIL-ARRAY. Set to NIL
+             ;; (transparent) if x,y is outside STENCIL-ARRAY or the
+             ;; array element is #xff, indicating full transparency.
+             (locally (declare (type stencil-array stencil-array))
+               (let ((stencil-x (+ stencil-dx x))
+                     (stencil-y (+ stencil-dy y)))
+                 (setf alpha
+                       (if (array-in-bounds-p stencil-array stencil-y stencil-x)
+                           (let ((value (aref stencil-array stencil-y stencil-x)))
+                             (if (= #xff value)
+                                 nil
+                                 value))
+                           nil)))))
+           (update-ink (i j)
+             (when (or (not ink) (not static-ink-p))
+               (setf ink      (clime:design-ink design i j)
+                     ink-rgba (typecase ink
+                                (standard-flipping-ink
+                                 (let ((d1 (slot-value ink 'climi::design1))
+                                       (d2 (slot-value ink 'climi::design2)))
+                                   (logior (logxor (climi::%rgba-value d1)
+                                                   (climi::%rgba-value d2))
+                                           #xff)))
+                                (t
+                                 (climi::%rgba-value ink)))))
+             (when (and (eq old-ink ink) (eql old-alpha alpha))
+               (return-from update-ink))
+
+             (cond ((typep ink 'standard-flipping-ink)
+                    ;; TODO this can be cached
+                    (let-rgba ((r.fg g.fg b.fg a.fg) ink-rgba)
+                      (setf source-r r.fg
+                            source-g g.fg
+                            source-b b.fg)
+                      (if alpha
+                          (setf source-a (octet-mult a.fg alpha)
+                                mode     :flipping/blend)
+                          (setf source-a    #x00
+                                source-rgba (%vals->rgba source-r source-g source-b #xff)
+                                mode        :flipping))))
+                   ((not alpha)
+                    (if (= #xff (logand #xff ink-rgba))
+                        (setf source-rgba ink-rgba
+                              mode        :copy)
+                        (let-rgba ((r g b a) ink-rgba)
+                          (setf source-r r
+                                source-g g
+                                source-b b
+                                source-a a
+                                mode     :blend))))
+                   (t
+                    (let-rgba ((r.fg g.fg b.fg a.fg) ink-rgba)
+                      (setf source-r r.fg
+                            source-g g.fg
+                            source-b b.fg
+                            source-a (octet-mult a.fg alpha)
+                            mode     (case source-a
+                                       (0
+                                        nil)
+                                       (255
+                                        (setf source-rgba ink-rgba)
+                                        :copy)
+                                       (t
+                                        :blend))))))))
+      (do-regions ((src-j j y y y2)
+                   (src-i i x x x2))
+        (when (or (null clip-region)
+                  (region-contains-position-p clip-region src-i src-j))
+          (when stencil-array
+            (update-alpha i j))
+          (update-ink i j)
+          (setf old-alpha alpha
+                old-ink   ink)
+
+          (case mode
+            (:flipping
+             (setf (aref dst-array j i) (logxor source-rgba
+                                                (aref dst-array j i))))
+            (:flipping/blend
+             (let-rgba ((r.bg g.bg b.bg a.bg) (aref dst-array j i))
+               (setf (aref dst-array j i)
+                     (octet-blend-function*
+                      (color-octet-xor source-r r.bg)
+                      (color-octet-xor source-g b.bg)
+                      (color-octet-xor source-b g.bg)
+                      source-a
+                      r.bg g.bg b.bg a.bg))))
+            (:copy
+             (setf (aref dst-array j i) source-rgba))
+            (:blend
+             (let-rgba ((r.bg g.bg b.bg a.bg) (aref dst-array j i))
+               (setf (aref dst-array j i)
+                     (octet-blend-function*
+                      source-r source-g source-b source-a
+                      r.bg     g.bg     b.bg     a.bg)))))))))
+  ;; XXX These #'1- are fishy. We don't capture correct region (rounding
+  ;; issue?). This problem is visible when scrolling.
+  (make-rectangle* (1- x) (1- y) (+ x width) (+ y height)))
+
+#+old (defun fill-image (image design &key (x 0) (y 0)
+                                     (width (pattern-width image))
+                                     (height (pattern-height image))
+                                     stencil (stencil-dx 0) (stencil-dy 0)
+                                     clip-region
+				&aux
+				(dst-array (climi::pattern-array image))
+				(x2 (+ x width -1))
+				(y2 (+ y height -1)))
   "Blends DESIGN onto IMAGE with STENCIL and a CLIP-REGION."
   (let ((stencil-array (and stencil (climi::pattern-array stencil))))
     (do-regions ((src-j j y y y2)
