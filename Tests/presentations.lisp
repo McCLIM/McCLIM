fe16387549c5d91e9e5b78462c93f24cb503dec7
Daniel Kochmański <daniel@turtleware.eu>
2020-06-24 14:03:35 +0200

    tests: presentations: add a new test and don't "hide" an issue
    One test was disabled despite correctly showing an issue in the
    presentation-subtypep. Also add a test which shows, that MEMBER and OR
    types do not mix well.

--- Tests/presentations.lisp
+++ Tests/presentations.lisp
@@ -105,4 +105,8 @@
   (expect-t-t '(or integer symbol) '(or symbol integer))
 
   (expect-t-t '(or real complex) 'number)
-  #+nil (expect-t-t 'number '(or real complex)))
+  (expect-t-t 'number '(or real complex)))
+
+(test presentations.type-relations.5
+  (let ((type (expand-presentation-type-abbreviation '(member "a" 3))))
+    (expect-t-t type '(or string integer))))
