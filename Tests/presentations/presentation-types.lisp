2532644464702b8572e03b024f6bd7cea45e1955
Daniel Kochmański <daniel@turtleware.eu>
2020-06-29 18:22:35 +0200

    tests: presentations: add new tests and move to a separate module
    - add a smoke test for the presentation method specializers
    - add a regression test for PRESENTATION-TYPEP

--- Tests/presentations/presentation-types.lisp
+++ Tests/presentations/presentation-types.lisp
@@ -110,3 +110,10 @@
 (test presentations.type-relations.5
   (let ((type (expand-presentation-type-abbreviation '(member "a" 3))))
     (fails (expect-t-t type '(or string integer)))))
+
+;;; This is a test for an issue, where a default method for
+;;; PRESENTATION-TYPEP was overwritten by a method specialized on the
+;;; type T (and returned incorrectly truth for unknown relations).
+(test presentations.typep.1
+  (define-presentation-type foo ())
+  (is (null (presentation-typep 3 'foo))))
