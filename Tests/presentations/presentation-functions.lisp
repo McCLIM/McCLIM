5f2a79f1f7be01f13a855188730accf9c1179991
Daniel Kochmański <daniel@turtleware.eu>
2020-07-01 15:07:27 +0200

    tests: define-presentation-method: test for style warnings
    When method [arguments / type parameters / type options] coincide, we
    signal a warning that the lexical variable is shadowed.

--- Tests/presentations/presentation-functions.lisp
+++ Tests/presentations/presentation-functions.lisp
@@ -20,3 +20,69 @@
     (is (equal '(:eql-t                    t) (call-spec                  t)))
     (is (equal '(:symbol              symbol) (call-spec             symbol)))
     (is (equal '(:seq-int (sequence integer)) (call-spec (sequence integer))))))
+
+(define-presentation-type pgf-warning.type1 ())
+(define-presentation-type pgf-warning.type2 (arg1)      :options (arg1 arg2))
+(define-presentation-type pgf-warning.type3 (type arg1) :options (arg1 arg2))
+(define-presentation-type pgf-warning.type4 ()          :options (type))
+(macrolet ((define-it (name args)
+             `(define-presentation-generic-function ,(gensym) ,name ,args)))
+  (define-it pgf-warning.fn1 (climi::type-key type))
+  (define-it pgf-warning.fn2 (climi::type-key climi::parameters type))
+  (define-it pgf-warning.fn3 (climi::type-key climi::parameters climi::options type)))
+
+(test pgf-warning
+  (macrolet ((signals-style-warning (expected form)
+               (let ((fail1 "~%~S~%~% not expected to signal STYLE-WARNING")
+                     (fail2 "~%~S~%~% expected to signal STYLE-WARNING"))
+                 `(let ((sig nil))
+                    (handler-bind ((style-warning
+                                     (lambda (c)
+                                       (setf sig t)
+                                       (muffle-warning c))))
+                      (compile nil '(lambda () ,form))
+                      (if (eq sig ,expected)
+                          (pass)
+                          (if sig
+                              (fail ,fail1 ',form)
+                              (fail ,fail2 ',form))))))))
+    ;; method/parameters/options doesn't coincide
+    (signals-style-warning
+     nil (define-presentation-method pgf-warning.fn1
+             ((type pgf-warning.type1))))
+    (signals-style-warning
+     nil (define-presentation-method pgf-warning.fn2
+             ((type pgf-warning.type1))))
+    (signals-style-warning
+     nil (define-presentation-method pgf-warning.fn3
+             ((type pgf-warning.type1))))
+    ;; parameters/options coincide
+    (signals-style-warning
+     nil (define-presentation-method pgf-warning.fn1
+             ((type pgf-warning.type2))))
+    (signals-style-warning
+     nil (define-presentation-method pgf-warning.fn2
+             ((type pgf-warning.type2))))
+    (signals-style-warning
+     t (define-presentation-method pgf-warning.fn3
+           ((type pgf-warning.type2))))
+    ;; method/parameters and parameters/options coincide
+    (signals-style-warning
+     nil (define-presentation-method pgf-warning.fn1
+             ((type pgf-warning.type3))))
+    (signals-style-warning
+     t (define-presentation-method pgf-warning.fn2
+             ((type pgf-warning.type3))))
+    (signals-style-warning
+     t (define-presentation-method pgf-warning.fn3
+           ((type pgf-warning.type3))))
+    ;; method/options coincide
+    (signals-style-warning
+     nil (define-presentation-method pgf-warning.fn1
+             ((type pgf-warning.type4))))
+    (signals-style-warning
+     nil (define-presentation-method pgf-warning.fn2
+           ((type pgf-warning.type4))))
+    (signals-style-warning
+     t (define-presentation-method pgf-warning.fn3
+           ((type pgf-warning.type4))))))
