40d7fd7534a44882586f554624b42c5cd04980c9
Daniel Kochmański <daniel@turtleware.eu>
2020-07-10 14:32:38 +0200

    tests: presentation-types: add tests for aggregate ptypes
    - stupid-subtypep is _not_ (or yesp (not surep))
    - add a comment explaining its behavior
    - fix old tests to account for aggregate presentaion types types
    - add tests for completion and subset-completion

--- Tests/presentations/presentation-types.lisp
+++ Tests/presentations/presentation-types.lisp
@@ -21,7 +21,7 @@
     ;; 23.8.4
     (pathname t)
     ;; 23.8.5
-    ((completion nil) t)
+    ((completion (42)) t)
     ;; not allowed abbreviations
     ;; (member t) ((member-sequence nil) t) ((member-alist nil) t)
     ((subset-completion nil) t)
@@ -36,11 +36,34 @@
     (expression t)
     (form expression t)))
 
+;;; The function STUPID-SUBTYPEP is used by translators to quickly determine
+;;; whether a particular translator is potentially applicable to a given
+;;; presentation (see 23.7.2 FIND-PRESENTATION-TRANSLATORS). Specification
+;;; says, that type parameters should not be taken into account. That leads to
+;;; a conclusion:
+;;;
+;;;   (stupid-subtypep a b) ; -> nil
+;;;
+;;; implies
+;;;
+;;;   (presentation-subtypep a b) ; -> (nil t)
+;;;
+;;; reverse implication is not true. For example:
+;;;
+;;;   (presentation-subtypep '(integer 1 2) '(integer 2 4)) ; -> (nil t)
+;;;   (stupid-subtypep       '(integer 1 2) '(integer 2 4)) ; -> t
+;;;
+;;; McCLIM makes an exception for types OR, AND, COMPLETION and
+;;; SUBSET-COMPLETION and sometimes accesses parameters. This is because
+;;; subtypep relation these cases may be an exception, because type A may be a
+;;; subtype of type B despite not inheriting from it.
+
 (defun expect-t-t (type supertype)
   (multiple-value-bind (yesp surep)
       (presentation-subtypep type supertype)
-    (is-true yesp)
-    (is-true surep))
+    (is (and yesp surep)
+        "Expected (T T), got (~s ~s)~%for ~s~%and ~s"
+        yesp surep type supertype))
   #+mcclim
   ;; we can do this because *presentation-type-supertypes* doesn't do
   ;; clever things with type parameters
@@ -49,18 +72,18 @@
 (defun expect-nil-t (type supertype)
   (multiple-value-bind (yesp surep)
       (presentation-subtypep type supertype)
-    (is-false yesp)
-    (is-true surep))
-  #+mcclim
-  (is-false (climi::stupid-subtypep type supertype)))
+    (is (and (null yesp) surep)
+        "Expected (NIL T), got (~s ~s)~%for ~s~%and ~s"
+        yesp surep type supertype)))
 
 (defun expect-nil-nil (type supertype)
   (multiple-value-bind (yesp surep)
       (presentation-subtypep type supertype)
-    (is-false yesp)
-    (is-false surep))
-  ;; stupid-subtypep must be conservative in what it reports as
-  ;; possibly acceptable.
+    (is (and (null yesp) (null surep))
+        "Expected (NIL NIL), got (~s ~s)~%for ~s~%and ~s"
+        yesp surep type supertype))
+  ;; stupid-subtypep must be conservative in what it reports as possibly
+  ;; acceptable.
   #+mcclim
   (is-true (climi::stupid-subtypep type supertype)))
 
@@ -72,13 +95,13 @@
   (loop for (type . supertypes) in *presentation-type-supertypes*
         do (expect-t-t type type)
         do (expect-t-t nil type)
-           ;; if presentation types were "real" (FIXME: work out what
-           ;; "real" means) types, then this wouldn't actually be true.
-           ;; However, PRESENTATION-SUBTYPEP works by walking up the type
-           ;; lattice until it finds a match, and only then checks the type
-           ;; parameters.  So even though presentation types (or
-           ;; abbreviations) like (MEMBER) actually denote the empty set,
-           ;; they are not PRESENTATION-SUBTYPEP NIL.
+           ;; The following assertion is not true for empty sets and meta
+           ;; types, but the test data does not contain such examples. For
+           ;; instance:
+           ;;
+           ;; (presentation-subtypep '(completion nil) nil) -> t t
+           ;; (presentation-subtypep '(and (integer 1 2)
+           ;;                              (integer 3 4)) nil) -> nil nil
         do (expect-nil-t type nil)
         do (mapcar (lambda (x) (expect-t-t type x)) supertypes)))
 
@@ -96,7 +119,10 @@
 (test presentations.type-relations.3
   (loop for (type) in *presentation-type-supertypes*
         do (expect-t-t `(and ,type (satisfies constantly-t)) type)
-        do (expect-nil-nil type `(and ,type (satisfies constantly-t)))
+        unless (eq (presentation-type-name type) 'completion)
+          ;; This assertion is not true for COMPLETION, because in this case
+          ;; we may test for each element.
+          do (expect-nil-nil type `(and ,type (satisfies constantly-t)))
         do (expect-t-t `(and ,type (not nil)) type)
         do (expect-nil-nil type `(and ,type (not nil)))))
 
@@ -110,6 +136,30 @@
 (test presentations.type-relations.5
   (let ((type (expand-presentation-type-abbreviation '(member "a" 3))))
     (fails (expect-t-t type '(or string integer)))))
+
+;;; COMPLETION
+(test presentations.type-relations.6
+  (expect-t-t '(completion (1 2 3)) 'integer)
+  (expect-nil-t '(completion (1 2 3)) 'string)
+  (expect-nil-t '(completion (1 2 3 "foo")) 'integer)
+  ;; See FIXME in presentation-type-functions.lisp for OR supertype.
+  (fails (expect-t-t '(completion (1 2 3 "foo")) '(or string integer)))
+  (expect-t-t '(completion (1 2 3)) '(and number (satisfies integerp)))
+  (expect-nil-t '(completion (1 2 3 3.14)) '(and number (satisfies integerp)))
+  (expect-t-t '(completion (3)) '(and (satisfies integerp)))
+  ;; completion vs completion
+  (expect-t-t '(completion (3)) '(completion (3 4 5)))
+  (expect-nil-t '(completion (3 4)) '(completion (3 5))))
+
+;;; SUBSET-COMPLETION
+(test presentations.type-relations.7
+  (expect-t-t '(subset-completion (1 2 3 4)) '(sequence integer))
+  (expect-nil-t '(subset-completion (1 2 3 4.3)) '(sequence integer))
+  (expect-t-t '(subset-completion (1 2 "fam")) '(sequence t))
+  (expect-nil-t '(subset-completion (1 2)) 'integer)
+  ;; subset-completion vs subset-completion
+  (expect-t-t '(subset-completion (1 2)) '(subset-completion (1 2 3)))
+  (expect-nil-t '(subset-completion (1 2)) '(subset-completion (1))))
 
 ;;; This is a test for an issue where a parametrized class without
 ;;; defined PRESENTATION-TYPEP method doesn't error.
