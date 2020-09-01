McCLIM/McCLIM

commit 116f0fb4633691036c39468f398929b5cf9f1a71

presentation-subtypep: add one more clause for completion supertype

Core/clim-core/presentations/presentation-type-functions.lisp
@@ -172,9 +172,16 @@ otherwise return false."
 ;;; all other types.  -- CSR, 2007-01-10
 ;;;
 ;;; (5) the COMPLETION presentation type ("one of") is equivalent to the
-;;;     Common Lisp type MEMBER. When it is used as MAYBE-SUBTYPE argument,
-;;;     then individual objects are used to determine its relation with
-;;;     MAYBE-SUPERTYPE. Otherwise "normal" rules apply.
+;;;     Common Lisp type MEMBER.
+;;;
+;;;     - when both types are COMPLETION then "normal" rules apply
+;;;
+;;;     - when MAYBE-SUBTYPE is COMPLETION then use individual objects to
+;;;       determine its relation with MAYBE-SUPERTYPE
+;;;
+;;;     - when MAYBE-SUPERTYPE is COMPLETION return (values t nil), because we
+;;;       can't tell whether the object presented with MAYBE-SUBTYPE is not
+;;;       one of the completion possibilities
 ;;;
 ;;; (6) the SUBSET-COMPLETION presentation type ("some of") doesn't have
 ;;;     equivalent Common Lisp type. When it is used as MAYBE-SUBTYPE and
@@ -273,6 +280,10 @@ otherwise return false."
                        (presentation-typep (funcall value-key elt) maybe-supertype))
                      sequence)
               t))))
+        ((and (not (eq maybe-subtype-name 'completion))
+              (eq maybe-supertype-name 'completion))
+         (return-from presentation-subtypep
+           (values t nil)))
         ((and (eq maybe-subtype-name 'subset-completion)
               (eq maybe-supertype-name 'sequence))
          (destructuring-bind (sequence

commit 4e06a53cd7be9c3d21a6dd0e8d0cf83498652379

presentation-subtypep: be conservative for the supertype completion
This commit fixes a problem introduced in 116f0fb4. Basically when we are not
sure if the type is a subtype of a particular completion we return

  (values nil nil)

instead of

  (values t nil)

This is to ensure, that in complete-input we do not match invalid
presentations.

Core/clim-core/presentations/presentation-type-functions.lisp
@@ -131,6 +131,13 @@ otherwise return false."
                         completion-type-parameters)
   t)
 
+(defun some-completion-item (function completion-type-parameters)
+  (map-completion-items (lambda (element)
+                          (when (funcall function element)
+                            (return-from some-completion-item t)))
+                        completion-type-parameters)
+  nil)
+
 ;;; PRESENTATION-SUBTYPEP suffers from some of the same problems as
 ;;; CL:SUBTYPEP, most (but sadly not all) of which were solved in H. Baker "A
 ;;; Decision Procedure for SUBTYPEP"; additionally, it suffers from the
@@ -287,7 +294,10 @@ otherwise return false."
         ((and (not (eq maybe-subtype-name 'completion))
               (eq maybe-supertype-name 'completion))
          (return-from presentation-subtypep
-           (values t nil)))
+           (values nil
+                   (not (some-completion-item
+                         (alexandria:rcurry #'presentation-typep maybe-subtype)
+                         maybe-supertype-parameters)))))
         ((and (eq maybe-subtype-name 'subset-completion)
               (eq maybe-supertype-name 'sequence))
          (let ((element-type (first maybe-supertype-parameters)))