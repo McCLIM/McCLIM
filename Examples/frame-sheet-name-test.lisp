c494876fca2bb6c42748579a4edaf79137ec869c
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2020-10-22 19:46:38 +0200

    examples: frame-sheet-name-test: separate package, use new file header format

--- Examples/frame-sheet-name-test.lisp
+++ Examples/frame-sheet-name-test.lisp
@@ -1,4 +1,24 @@
-(cl:in-package #:clim-demo)
+;;; ---------------------------------------------------------------------------
+;;;   License: LGPL-2.1+ (See file 'Copyright' for details).
+;;; ---------------------------------------------------------------------------
+;;;
+;;;  (c) copyright 2020 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+;;;
+;;; ---------------------------------------------------------------------------
+;;;
+;;; This program demonstrates how to modify the pretty name of an
+;;; application frame (and the pretty name of the corresponding
+;;; top-level sheet). Depending on the backend and windowing
+;;; environment, these changes may or may not be immediately picked up
+;;; and displayed by the window manager and/or graphical environment.
+
+(cl:defpackage #:clim-demo.names-and-icons
+  (:use
+   #:clim-lisp
+   #:clim)
+  (:export
+   #:frame-sheet-name-test))
+(cl:in-package #:clim-demo.names-and-icons)
 
 (define-application-frame frame-sheet-name-test ()
   ()
@@ -6,13 +26,13 @@
   (:pane
    (let* ((frame *application-frame*)
           (sheet (frame-top-level-sheet frame)))
-     (outlining (:thickness 10 :background +gray84+)
+     (spacing (:thickness 10)
        (tabling (:min-width 600 :spacing 8)
          (list (labelling (:label " ")) ; work around problems with empty labels for now
                (labelling (:label "Name"))
                (labelling (:label "Pretty Name")))
          (list (labelling (:label "Sheet"))
-               (make-pane :label :label (prin1-to-string (sheet-name sheet)))
+               (make-pane :label :label (prin1-to-string (clime:sheet-name sheet)))
                (outlining (:thickness 4 :background +white+)
                  (make-pane :text-field :name 'sheet-pretty-name
                                         :value (clime:sheet-pretty-name sheet)
