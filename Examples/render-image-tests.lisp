b2b867b8fd63d28e2e203c293cc5ea7a3297973f
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2020-10-26 23:50:06 +0100

    examples: render-image-tests: use new file header format, untabify, cleanup

--- Examples/render-image-tests.lisp
+++ Examples/render-image-tests.lisp
@@ -1,4 +1,14 @@
-(in-package :clim-demo)
+;;; ---------------------------------------------------------------------------
+;;;   License: LGPL-2.1+ (See file 'Copyright' for details).
+;;; ---------------------------------------------------------------------------
+;;;
+;;;  (c) copyright 2017 Alessandro Serra <gas2serra@gmail.com>
+;;;  (c) copyright 2018 Daniel Kochmanski <daniel@turtleware.eu>
+;;;  (c) copyright 2020 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+;;;
+;;; ---------------------------------------------------------------------------
+
+(in-package #:clim-demo)
 
 (defparameter *render-image-tests* (make-hash-table :test 'equal))
 
@@ -6,14 +16,17 @@
 (defparameter *render-image-height* 700)
 (defparameter *render-image-border-width* 5)
 
-(defparameter *testing-image-directory* (uiop/pathname:merge-pathnames* "Examples/images/" (asdf:system-source-directory (asdf:find-system :mcclim))))
-(defparameter *testing-image-files* '("RGBXPLORER8.png"
-                                      "White_Balance_RGB.png"
-                                      "MicroGrayTest.png"))
+(defparameter *testing-image-directory*
+  (uiop/pathname:merge-pathnames*
+   "Examples/images/" (asdf:system-source-directory (asdf:find-system :mcclim))))
 
 (defparameter *testing-image-rgb-file* "RGBXPLORER8.png")
 (defparameter *testing-image-bn1-file* "White_Balance_RGB.png")
 (defparameter *testing-image-bn2-file* "MicroGrayTest.png")
+(defparameter *testing-image-files*
+  (list *testing-image-rgb-file*
+        *testing-image-bn1-file*
+        *testing-image-bn2-file*))
 
 (defstruct render-image-test name description drawer)
 
@@ -41,87 +54,85 @@
    (selector :list-pane
              :mode :exclusive
              :name-key #'render-image-test-name
-             :items (sort (loop for x being the hash-values of *render-image-tests*
-                                collect x) #'string< :key #'render-image-test-name)
+             :items (sort (alexandria:hash-table-values *render-image-tests*)
+                          #'string< :key #'render-image-test-name)
              :value-changed-callback #'render-image-update-selection)
    (condition-option
-    (clim:with-radio-box (:orientation :vertical
-                                       :value-changed-callback 'render-image-update-condition-option)
-      (clim:radio-box-current-selection "message")
+    (with-radio-box (:orientation :vertical
+                     :value-changed-callback 'render-image-update-condition-option)
+      (radio-box-current-selection "message")
       "break")))
   (:layouts
    (default
-       (spacing (:thickness 3)
-         (horizontally ()
-           (vertically ()
-             (spacing (:thickness 3)
-               (clim-extensions:lowering ()
-                 (scrolling (:scroll-bar :vertical :height *render-image-height*) selector)))
-             (labelling (:label "Condition")
-               condition-option))
-           (vertically ()
-             (spacing (:thickness 3)
-               (clim-extensions:lowering ()
-                 (horizontally ()
-                   (labelling (:label "Stream")
-                     (climi::bordering (:border-width *render-image-border-width*)
-                       output)))))
-             (spacing (:thickness 3)
-               (clim-extensions:lowering ()
-                 (scrolling (:scroll-bar :vertical :height 200) description)))))))))
+    (spacing (:thickness 3)
+      (horizontally ()
+        (vertically ()
+          (spacing (:thickness 3)
+            (clim-extensions:lowering ()
+              (scrolling (:scroll-bar :vertical :height *render-image-height*) selector)))
+          (labelling (:label "Condition")
+            condition-option))
+        (vertically ()
+          (spacing (:thickness 3)
+            (clim-extensions:lowering ()
+              (horizontally ()
+                (labelling (:label "Stream")
+                  (outlining (:thickness *render-image-border-width*)
+                    output)))))
+          (spacing (:thickness 3)
+            (clim-extensions:lowering ()
+              (scrolling (:scroll-bar :vertical :height 200) description)))))))))
 
 (defun render-image-update-condition-option (this-gadget selected-gadget)
   (declare (ignore this-gadget))
-  (with-slots (signal-condition-p) clim:*application-frame*
+  (with-slots (signal-condition-p) *application-frame*
     (setf signal-condition-p
-	  (string= (clim:gadget-label selected-gadget) "break"))))
+          (string= (gadget-label selected-gadget) "break"))))
 
 (defun render-image-update-selection (pane item)
   (declare (ignore pane))
-  (with-slots (current-selection) clim:*application-frame*
-    (setf current-selection item))
-  (window-clear (get-frame-pane *application-frame* 'description))
-  (redisplay-frame-pane *application-frame* (get-frame-pane *application-frame* 'output) :force-p t))
+  (let ((frame *application-frame*))
+    (with-slots (current-selection) frame
+      (setf current-selection item))
+    (window-clear (get-frame-pane frame 'description))
+    (redisplay-frame-pane frame (get-frame-pane frame 'output) :force-p t)))
 
 (defun render-image-display-output (frame pane)
   (declare (ignore pane))
   (let ((output (get-frame-pane frame 'output))
         (item (slot-value frame 'current-selection)))
-    (let ((description (get-frame-pane *application-frame* 'description)))
+    (let ((description (get-frame-pane frame 'description)))
       (when item
         (with-text-style (description (make-text-style :sans-serif :roman :normal))
           (write-string (render-image-test-description item) description)
           (terpri description))
-        (if (slot-value *application-frame* 'signal-condition-p)
-            (clim:with-drawing-options (output :clipping-region
-                                               (clim:make-rectangle* 0 0 *render-image-width* *render-image-height*))
-              (clim:draw-rectangle* output 0 0 *render-image-width* *render-image-height* :filled t
-                                    :ink clim:+grey90+)
+        (if (slot-value frame 'signal-condition-p)
+            (with-drawing-options (output :clipping-region
+                                          (make-rectangle* 0 0 *render-image-width* *render-image-height*))
+              (draw-rectangle* output 0 0 *render-image-width* *render-image-height* :filled t
+                                                                                     :ink +grey90+)
               (funcall (render-image-test-drawer item) output))
             (handler-case
-                (clim:with-drawing-options (output :clipping-region
-                                                   (clim:make-rectangle* 0 0 *render-image-width* *render-image-height*))
-                  (clim:draw-rectangle* output 0 0 *render-image-width* *render-image-height* :filled t
-                                        :ink clim:+grey90+)
+                (with-drawing-options (output :clipping-region
+                                              (make-rectangle* 0 0 *render-image-width* *render-image-height*))
+                  (draw-rectangle* output 0 0 *render-image-width* *render-image-height* :filled t
+                                                                                         :ink +grey90+)
                   (funcall (render-image-test-drawer item) output))
               (condition (condition)
-                (clim:with-drawing-options (description :ink +red+)
+                (with-drawing-options (description :ink +red+)
                   (format description "Error:~a~%" condition)))))))))
 
-
 (defun run-render-image-tests ()
-  (run-frame-top-level
-   (make-application-frame
-    'render-image-tests)))
+  (run-frame-top-level (make-application-frame 'render-image-tests)))
 
 (defun render-image-test-make-rgba-image (w h color)
   (let* ((image (make-instance 'climi::%rgba-pattern
                                :array (make-array (list h w)
                                                   :element-type '(unsigned-byte 32)
                                                   :initial-element #xFFFFFF00)))
-         (pixels (climi::pattern-array image)))
-    (dotimes (x w)
-      (dotimes (y h)
+         (pixels (clime:pattern-array image)))
+    (dotimes (y h)
+      (dotimes (x w)
         (setf (aref pixels y x) (climi::%rgba-value color))))
     image))
 
@@ -169,14 +180,14 @@
   (let ((path (uiop/pathname:merge-pathnames* *testing-image-rgb-file* *testing-image-directory*)))
     (let ((image (make-pattern-from-bitmap-file path)))
       (with-bounding-rectangle* (x1 y1 x2 y2)
-          clipping-region
+                                clipping-region
         (draw-rectangle* stream x1 y1 x2 y2 :ink +green+ :filled nil))
       (with-bounding-rectangle* (x1 y1 x2 y2)
-          (transform-region transformation clipping-region)
+                                (transform-region transformation clipping-region)
         (draw-rectangle* stream x1 y1 x2 y2 :ink +blue+ :filled nil))
       (clim-render:draw-image* stream image 0 0
-                              :transformation transformation
-                              :clipping-region clipping-region))))
+                               :transformation transformation
+                               :clipping-region clipping-region))))
 
 (defun render-image-test-08 (stream h)
   (let ((path (uiop/pathname:merge-pathnames* *testing-image-rgb-file* *testing-image-directory*)))
@@ -192,14 +203,14 @@
   (let ((path (uiop/pathname:merge-pathnames* *testing-image-rgb-file* *testing-image-directory*)))
     (let ((image (make-pattern-from-bitmap-file path)))
       (with-bounding-rectangle* (x1 y1 x2 y2)
-          clipping-region
+                                clipping-region
         (draw-rectangle* stream x1 y1 x2 y2 :ink +green+ :filled nil))
       (with-bounding-rectangle* (x1 y1 x2 y2)
-          (transform-region transformation clipping-region)
+                                (transform-region transformation clipping-region)
         (draw-rectangle* stream x1 y1 x2 y2 :ink +blue+ :filled nil))
       (draw-design stream image :x 0 :y 0
-                   :transformation transformation
-                   :clipping-region clipping-region))))
+                                :transformation transformation
+                                :clipping-region clipping-region))))
 
 (defun render-image-test-16 (stream design cx cy cw ch w h)
   (let ((path (uiop/pathname:merge-pathnames* *testing-image-rgb-file* *testing-image-directory*)))
@@ -230,12 +241,12 @@ purple, olive."
 
 (define-render-image-test "2d - 05) translate" (stream)
     ""
-  (render-image-test-05 stream (clim:make-translation-transformation 10 10)))
+  (render-image-test-05 stream (make-translation-transformation 10 10)))
 
 (define-render-image-test "2d - 06) clipping" (stream)
     ""
   (render-image-test-06 stream
-                        (clim:make-translation-transformation 10 10)
+                        (make-translation-transformation 10 10)
                         (make-rectangle* 50 50 250 250)))
 
 (define-render-image-test "2d - 07) with-translation" (stream)
@@ -249,12 +260,12 @@ purple, olive."
 
 (define-render-image-test "2d - 09) design translate" (stream)
     ""
-  (render-image-test-09 stream (clim:make-translation-transformation 10 10)))
+  (render-image-test-09 stream (make-translation-transformation 10 10)))
 
 (define-render-image-test "2d - 10) design clipping" (stream)
     ""
   (render-image-test-10 stream
-                        (clim:make-translation-transformation 10 10)
+                        (make-translation-transformation 10 10)
                         (make-rectangle* 50 50 250 250)))
 
 (define-render-image-test "2d - 11) design with-translation" (stream)
@@ -274,12 +285,12 @@ purple, olive."
                         10 300))
 
 ;;;
-;;; indipendent
+;;; independent
 ;;;
 
 (define-render-image-test "zz - 01) output record :draw nil" (stream)
     ""
-  (clim:with-output-to-output-record (stream)
+  (with-output-to-output-record (stream)
     (with-output-recording-options (stream :record t :draw nil)
       (render-image-test-08 stream 10)
       (draw-rectangle* stream 10 10 50 50 :ink +green+ :filled t))))
@@ -287,9 +298,9 @@ purple, olive."
 (define-render-image-test "zz - 01) output record moving" (stream)
     ""
   (let ((record
-         (clim:with-output-to-output-record (stream)
-           (with-output-recording-options (stream :record t :draw t)
-             (render-image-test-08 stream 10)
-             (draw-rectangle* stream 10 10 50 50 :ink +green+ :filled t)))))
-    (setf (clim:output-record-position record) (values 10 310))
+          (with-output-to-output-record (stream)
+            (with-output-recording-options (stream :record t :draw t)
+              (render-image-test-08 stream 10)
+              (draw-rectangle* stream 10 10 50 50 :ink +green+ :filled t)))))
+    (setf (output-record-position record) (values 10 310))
     (replay record stream)))
