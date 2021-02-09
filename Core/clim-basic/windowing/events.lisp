f486732ffc9af0038b9bf8d074f559ff154b78db
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2021-02-09 01:55:12 +0100

    windowing: events: rename pointer click events to match specification
    POINTER-{BUTTON-CLICK -> CLICK}-EVENT
    POINTER-{BUTTON-DOUBLE-CLICK -> DOUBLE-CLICK}-EVENT
    POINTER-{BUTTON-CLICK-AND-HOLD -> CLICK-AND-HOLD}-EVENT
    
    Those event class are not currently used in McCLIM.

--- Core/clim-basic/windowing/events.lisp
+++ Core/clim-basic/windowing/events.lisp
@@ -190,12 +190,12 @@
   ((button :initarg :button
            :reader pointer-event-button)))
 
-(define-event-class pointer-button-press-event          (pointer-button-event) ())
-(define-event-class pointer-button-release-event        (pointer-button-event) ())
-(define-event-class pointer-button-hold-event           (pointer-button-event) ())
-(define-event-class pointer-button-click-event          (pointer-button-event) ())
-(define-event-class pointer-button-double-click-event   (pointer-button-event) ())
-(define-event-class pointer-button-click-and-hold-event (pointer-button-event) ())
+(define-event-class pointer-button-press-event   (pointer-button-event) ())
+(define-event-class pointer-button-release-event (pointer-button-event) ())
+(define-event-class pointer-button-hold-event    (pointer-button-event) ())
+(define-event-class pointer-click-event          (pointer-button-event) ())
+(define-event-class pointer-double-click-event   (pointer-button-event) ())
+(define-event-class pointer-click-and-hold-event (pointer-button-event) ())
 
 (define-event-class pointer-scroll-event (pointer-button-event)
   ((delta-x :initform 0 :initarg :delta-x
