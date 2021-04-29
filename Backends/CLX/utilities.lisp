923bd95534ca7301146cba6e7ff915850cb82ea2
Daniel Kochmański <daniel@turtleware.eu>
2021-04-29 15:58:02 +0200

    clx: round-coordinate: correct the comment and add one case
    The comment incorrectly stated that we use "mercantile rounding" otherwise known
    as "round half away from zero".
    
    Also add a case for long-floats. While sbcl doesn't have a specialized
    representation for this type other implementations may have (like ecl).

--- Backends/CLX/utilities.lisp
+++ Backends/CLX/utilities.lisp
@@ -3,22 +3,21 @@
 (declaim (inline round-coordinate))
 (defun round-coordinate (x)
   "Function used for rounding coordinates."
-  ;; We use "mercantile rounding", instead of the CL round to nearest
-  ;; even number, when in doubt.
+  ;; When in doubt we use "round half up" rounding, instead of the CL:ROUND
+  ;; "round half to even".
   ;;
-  ;; Reason: As the CLIM drawing model is specified, you quite often
-  ;; want to operate with coordinates, which are multiples of 1/2.
-  ;; Using CL:ROUND gives you "random" results. Using "mercantile
-  ;; rounding" gives you consistent results.
+  ;; Reason: As the CLIM drawing model is specified, you quite often want to
+  ;; operate with coordinates, which are multiples of 1/2.  Using CL:ROUND gives
+  ;; "random" results. Using "round half up" gives you more consistent results.
   ;;
-  ;; Note that CLIM defines pixel coordinates to be at the corners,
-  ;; while in X11 they are at the centers. We don't do much about the
-  ;; discrepancy, but rounding up at half pixel boundaries seems to
-  ;; work well.
+  ;; Note that CLIM defines pixel coordinates to be at the corners, while in X11
+  ;; they are at the centers. We don't do much about the discrepancy, but
+  ;; rounding up at half pixel boundaries seems to work well.
   (etypecase x
     (integer      x)
     (single-float (values (floor (+ x .5f0))))
     (double-float (values (floor (+ x .5d0))))
+    (long-float   (values (floor (+ x .5l0))))
     (ratio        (values (floor (+ x 1/2))))))
 
 (defmacro with-round-positions ((transformation &rest coordinates) &body body)
