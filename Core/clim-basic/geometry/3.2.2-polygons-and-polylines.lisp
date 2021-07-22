26034d94226a7df6a0d058ddf3a9220ed1459c77
Daniel Kochmański <daniel@turtleware.eu>
2021-07-22 21:12:48 +0200

    geometry: refactor regions
    - move each "other region" class to a separate file
    - move bounding rectangle methods to their files
    - move transform region methods to their files
    - move region-contains-position-p methods to their files
    - rearrange the file regions.lisp

---
+++ Core/clim-basic/geometry/3.2.2-polygons-and-polylines.lisp
@@ -0,0 +1,161 @@
+;;; ---------------------------------------------------------------------------
+;;;   License: LGPL-2.1+ (See file 'Copyright' for details).
+;;; ---------------------------------------------------------------------------
+;;;
+;;;  (c) copyright 1998-2002 Gilbert Baumann <gbaumann@common-lisp.net>
+;;;  (c) copyright 2001 Arnaud Rouanet <rouanet@emi.u-bordeaux.fr>
+;;;  (c) copyright 2001 Julien Boninfan
+;;;  (c) copyright 2002-2004 Timothy Moore <tmoore@common-lisp.net>
+;;;  (c) copyright 2002 Alexey Dejneka
+;;;  (c) copyright 2004-2009 Andy Hefner <ahefner@common-lisp.net>
+;;;  (c) copyright 2006-2008 Christophe Rhodes <crhodes@common-lisp.net>
+;;;  (c) copyright 2014-2016 Robert Strandh <robert.strandh@gmail.com>
+;;;  (c) copyright 2017 Peter <craven@gmx.net>
+;;;  (c) copyright 2017-2019 Daniel Kochmański <daniel@turtleware.eu>
+;;;  (c) copyright 2017,2018 Cyrus Harmon <cyrus@bobobeach.com>
+;;;  (c) copyright 2018,2021 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+;;;
+;;; ----------------------------------------------------------------------------
+;;;
+;;; 3.2.2 Polygons and Polylines
+
+(in-package #:clim-internals)
+
+(defclass standard-polyline (cached-bbox-mixin polyline)
+  ((points :initarg :points :reader polygon-points)
+   (closed :initarg :closed)))
+
+(defclass standard-polygon (cached-bbox-mixin polygon)
+  ((points :initarg :points :reader polygon-points)))
+
+(defmethod slots-for-pprint-object append ((object standard-polyline))
+  '(points closed))
+
+(defmethod print-object ((region standard-polyline) sink)
+  (maybe-print-readably (region sink)
+    (print-unreadable-object (region sink :identity t :type t))))
+
+(defun make-polyline (point-seq &key closed)
+  (assert (every #'pointp point-seq))
+  (setq point-seq (remove-duplicated-points point-seq closed))
+  (if (< (length point-seq) 2)
+      +nowhere+
+      (make-instance 'standard-polyline :points point-seq :closed closed)))
+
+(defun make-polyline* (coord-seq &key closed)
+  (make-polyline (coord-seq->point-seq coord-seq) :closed closed))
+
+(defun make-polygon (point-seq)
+  (assert (every #'pointp point-seq))
+  (setq point-seq (remove-duplicated-points point-seq t))
+  (if (< (length point-seq) 3)
+      +nowhere+
+      (make-instance 'standard-polygon :points point-seq)))
+
+(defun make-polygon* (coord-seq)
+  (make-polygon (coord-seq->point-seq coord-seq)))
+
+(defun make-regular-polygon (cx cy r n)
+  (collect (coords)
+    (loop repeat n
+          with x0 = cx
+          with y0 = (- cy r)
+          for angle from 0 by (/ (* 2 pi) n)
+          for tr = (make-rotation-transformation* angle cx cy)
+          do (multiple-value-bind (x1 y1) (transform-position tr x0 y0)
+               (coords (make-point x1 y1))))
+    (make-polygon (coords))))
+
+(defmethod map-over-polygon-coordinates (fun (region standard-polygon))
+  (with-slots (points) region
+    (mapc (lambda (p) (funcall fun (point-x p) (point-y p))) points)))
+
+(defmethod map-over-polygon-segments (fun (region standard-polygon))
+  (with-slots (points) region
+    (do ((q points (cdr q)))
+        ((null (cdr q))
+         (funcall fun
+                  (point-x (car q)) (point-y (car q))
+                  (point-x (car points)) (point-y (car points))))
+      (funcall fun
+               (point-x (car q)) (point-y (car q))
+               (point-x (cadr q)) (point-y (cadr q))))))
+
+(defmethod map-over-polygon-coordinates (fun (region standard-polyline))
+  (with-slots (points) region
+    (mapc (lambda (p) (funcall fun (point-x p) (point-y p))) points)))
+
+(defmethod map-over-polygon-segments (fun (region standard-polyline))
+  (with-slots (points closed) region
+    (do ((q points (cdr q)))
+        ((null (cdr q))
+         (when closed
+           (funcall fun
+                    (point-x (car q)) (point-y (car q))
+                    (point-x (car points)) (point-y (car points)))))
+      (funcall fun (point-x (car q)) (point-y (car q))
+               (point-x (cadr q)) (point-y (cadr q))))))
+
+(defmethod polyline-closed ((region standard-polyline))
+  (with-slots (closed) region
+    closed))
+
+(defmethod region-contains-position-p ((region polyline) x y)
+  (setf x (coordinate x)
+        y (coordinate y))
+  (block nil
+    (map-over-polygon-segments
+     (lambda (x1 y1 x2 y2)
+       (when (segment-contains-point-p x1 y1 x2 y2 x y)
+         (return t)))
+     region)
+    nil))
+
+(defmethod region-contains-position-p ((region polygon) x y)
+  (let ((insidep nil))
+    (and (region-contains-position-p (bounding-rectangle region) x y)
+         (progn
+           (map-over-polygon-segments
+            (lambda (x1 y1 x2 y2)
+              (when (segment-contains-point-p x1 y1 x2 y2 x y)
+                (return-from region-contains-position-p t))
+              (unless (< y1 y2)
+                (rotatef y1 y2)
+                (rotatef x1 x2))
+              (when (and (not (eq (> y1 y) (> y2 y)))
+                         (minusp (line-equation x1 y1 x2 y2 x y)))
+                (setf insidep (not insidep))))
+            region)
+           insidep))))
+
+(defun polygon-bounding-rectangle (region)
+  (loop for point in (polygon-points region)
+        for (x y) = (multiple-value-list (point-position point))
+        minimizing x into x1 maximizing x into x2
+        minimizing y into y1 maximizing y into y2
+        finally (return (values x1 y1 x2 y2))))
+
+(defmethod bounding-rectangle* ((region standard-polyline))
+  (polygon-bounding-rectangle region))
+
+(defmethod bounding-rectangle* ((region standard-polygon))
+  (polygon-bounding-rectangle region))
+
+(defmethod transform-region (transformation (region standard-polyline))
+  (with-slots (points closed) region
+    (make-polyline
+     (mapcar (lambda (p)
+               (multiple-value-bind (x* y*)
+                   (transform-position transformation (point-x p) (point-y p))
+                 (make-point x* y*)))
+             points)
+     :closed closed)))
+
+(defmethod transform-region (transformation (region standard-polygon))
+  (with-slots (points) region
+    (make-polygon
+     (mapcar (lambda (p)
+               (multiple-value-bind (x* y*)
+                   (transform-position transformation (point-x p) (point-y p))
+                 (make-point x* y*)))
+             points))))
---
+++ Core/clim-basic/geometry/3.2.3-lines.lisp
@@ -0,0 +1,100 @@
+;;; ---------------------------------------------------------------------------
+;;;   License: LGPL-2.1+ (See file 'Copyright' for details).
+;;; ---------------------------------------------------------------------------
+;;;
+;;;  (c) copyright 1998-2002 Gilbert Baumann <gbaumann@common-lisp.net>
+;;;  (c) copyright 2001 Arnaud Rouanet <rouanet@emi.u-bordeaux.fr>
+;;;  (c) copyright 2001 Julien Boninfan
+;;;  (c) copyright 2002-2004 Timothy Moore <tmoore@common-lisp.net>
+;;;  (c) copyright 2002 Alexey Dejneka
+;;;  (c) copyright 2004-2009 Andy Hefner <ahefner@common-lisp.net>
+;;;  (c) copyright 2006-2008 Christophe Rhodes <crhodes@common-lisp.net>
+;;;  (c) copyright 2014-2016 Robert Strandh <robert.strandh@gmail.com>
+;;;  (c) copyright 2017 Peter <craven@gmx.net>
+;;;  (c) copyright 2017-2019 Daniel Kochmański <daniel@turtleware.eu>
+;;;  (c) copyright 2017,2018 Cyrus Harmon <cyrus@bobobeach.com>
+;;;  (c) copyright 2018,2021 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+;;;
+;;; ----------------------------------------------------------------------------
+;;;
+;;; 3.2.3 Lines
+
+(in-package #:clim-internals)
+
+(defclass standard-line (line)
+  ((x1 :type coordinate :initarg :x1)
+   (y1 :type coordinate :initarg :y1)
+   (x2 :type coordinate :initarg :x2)
+   (y2 :type coordinate :initarg :y2)))
+
+(defmethod slots-for-pprint-object append ((object standard-line))
+  '(x1 y1 x2 y2))
+
+(defmethod print-object ((region standard-line) sink)
+  (maybe-print-readably (region sink)
+    (print-unreadable-object (region sink :identity nil :type t)
+         (with-slots (x1 y1 x2 y2) region
+           (format sink "~D ~D ~D ~D" x1 y1 x2 y2)))))
+
+(defun make-line (start-point end-point)
+  (make-line* (point-x start-point) (point-y start-point)
+              (point-x end-point) (point-y end-point)))
+
+(defun make-line* (start-x start-y end-x end-y)
+  (setf start-x (coordinate start-x)
+        start-y (coordinate start-y)
+        end-x (coordinate end-x)
+        end-y (coordinate end-y))
+  (if (and (coordinate= start-x end-x)
+           (coordinate= start-y end-y))
+      +nowhere+
+      (make-instance 'standard-line :x1 start-x :y1 start-y :x2 end-x :y2 end-y)))
+
+(defmethod line-start-point* ((line standard-line))
+  (with-slots (x1 y1 x2 y2) line
+    (values x1 y1)))
+
+(defmethod line-end-point* ((line standard-line))
+  (with-slots (x1 y1 x2 y2) line
+    (values x2 y2)))
+
+(defmethod line-start-point ((line line))
+  (multiple-value-bind (x y) (line-start-point* line)
+    (make-point x y)))
+
+(defmethod line-end-point ((line line))
+  (multiple-value-bind (x y) (line-end-point* line)
+    (make-point x y)))
+
+;;; Polyline protocol for standard-line's
+
+(defmethod polygon-points ((line standard-line))
+  (with-slots (x1 y1 x2 y2) line
+    (list (make-point x1 y1) (make-point x2 y2))))
+
+(defmethod map-over-polygon-coordinates (fun (line standard-line))
+  (with-slots (x1 y1 x2 y2) line
+    (funcall fun x1 y1)
+    (funcall fun x2 y2)))
+
+(defmethod map-over-polygon-segments (fun (line standard-line))
+  (with-slots (x1 y1 x2 y2) line
+    (funcall fun x1 y1 x2 y2)))
+
+(defmethod polyline-closed ((line standard-line))
+  nil)
+
+(defmethod region-contains-position-p ((region line) x y)
+  (multiple-value-bind (x1 y1) (line-start-point* region)
+    (multiple-value-bind (x2 y2) (line-end-point* region)
+      (segment-contains-point-p x1 y1 x2 y2 x y))))
+
+(defmethod bounding-rectangle* ((a standard-line))
+  (with-slots (x1 y1 x2 y2) a
+    (values (min x1 x2) (min y1 y2) (max x1 x2) (max y1 y2))))
+
+(defmethod transform-region (transformation (line standard-line))
+  (with-slots (x1 y1 x2 y2) line
+    (multiple-value-bind (x1* y1*) (transform-position transformation x1 y1)
+      (multiple-value-bind (x2* y2*) (transform-position transformation x2 y2)
+        (make-line* x1* y1* x2* y2*)))))
