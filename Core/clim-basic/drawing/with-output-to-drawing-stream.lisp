e59c8bfb832748fde7c953bda7b3197367c08539
Daniel Kochmański <daniel@turtleware.eu>
2022-03-30 11:59:50 +0200

    core: with-output-to-drawing-stream: use WITH-PORT
    This approach ensures that when the port was created fro this particular
    invocation then it is destroyed afterwards. It is still possible to retain the
    port (per WITH-PORT behavior), by supplying a port that already exists to the
    macro.
    
    Additionally make the NULL specialization a little more robust - when the
    destination is NIL and there is no continuation nor backend specialization, then
    we use FIND-PORT - we don't want to destroy the interactive port after the
    window is created, because that would also destroy the window. We also specify
    the window input buffer to nil to avoid race conditions with the event queue.

--- Core/clim-basic/drawing/with-output-to-drawing-stream.lisp
+++ Core/clim-basic/drawing/with-output-to-drawing-stream.lisp
@@ -7,10 +7,11 @@
        (invoke-with-output-to-drawing-stream (function ,cont) ,backend ,destination ,@args))))
 
 (defmethod invoke-with-output-to-drawing-stream (continuation backend destination &rest args)
-  (let ((port (find-port :server-path backend)))
+  (with-port (port backend)
     (apply #'invoke-with-output-to-drawing-stream continuation port destination args)))
 
-(defmethod invoke-with-output-to-drawing-stream (cont (backend basic-port) (window null) &rest args)
-  (let ((window (apply #'open-window-stream :port backend args)))
-    (multiple-value-prog1 (funcall cont window)
+(defmethod invoke-with-output-to-drawing-stream (continuation backend (window null) &rest args)
+  (let* ((port (find-port :server-path backend))
+         (window (apply #'open-window-stream :port port :input-buffer nil args)))
+    (multiple-value-prog1 (funcall continuation window)
       (finish-output window))))
