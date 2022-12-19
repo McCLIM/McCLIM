0639f2e7dcc2d1a810b64f64ae70f78b10d57284
Daniel Kochmański <daniel@turtleware.eu>
2022-12-19 23:58:58 +0100

    documentation: document pointer cursors and standard key names

--- Backends/CLX/cursor.lisp
+++ Backends/CLX/cursor.lisp
@@ -1,42 +1,45 @@
 (in-package #:clim-clx)
 
 (defvar *clx-cursor-mapping*
-  '(;; These are taken from the Franz CLIM User's Guide
-    (:busy 150)
-    (:button 60)
-    (:default 68)
-    (:horizontal-scroll 108)
-    (:horizontal-thumb 108)
-    (:lower-left 12)
-    (:lower-right 14)
-    (:move 52)
-    (:position 130)
-    (:prompt 152)
-    (:scroll-down 106)
-    (:scroll-left 110)
-    (:scroll-right 112)
-    (:scroll-up 114)
-    (:upper-left 134)
-    (:upper-right 136)
-    (:vertical-scroll 116)
-    (:vertical-thumb 116)
+  `(((:default :arrow) 68)
+    ((:prompt :i-beam) 152)
+    ((:button :hand) 60)
+    ((:busy :wait) 150)
+    ((:not-allowed) 88)
+    ((:position) 130)
+    ((:move) 52)
+    ((:arrow-we) 108)
+    ((:arrow-ns) 116)
+    ((:grab) 60)
+    ((:help) 92)
+    ;; Backward compatibility (Franz)
+    ((:horizontal-scroll :horizontal-thumb) 108)
+    ((:vertical-scroll :vertical-thumb) 116)
+    ((:lower-left) 12)
+    ((:lower-right) 14)
+    ((:scroll-down) 106)
+    ((:scroll-left) 110)
+    ((:scroll-right) 112)
+    ((:scroll-up) 114)
+    ((:upper-left) 134)
+    ((:upper-right) 136)
     ;; The following are not in the Franz docs, but might be useful.
-    (:i-beam 152)
-    (:vertical-pointer 22)
-    (:pencil 86)
-    (:rotate 50)
-    (:choose 60)))
+    ((:vertical-pointer) 22)
+    ((:pencil) 86)
+    ((:rotate) 50)
+    ((:choose) 60)))
 
 (defun make-cursor-table (port)
   (declare (optimize (safety 3) (debug 3) (speed 0) (space 0)))
   (let ((font (xlib:open-font (clx-port-display port) "cursor")))
-    (loop for (symbol code) in *clx-cursor-mapping*
-          do (setf (gethash symbol (clx-port-cursor-table port))
-                   (xlib:create-glyph-cursor
-                    :foreground (xlib:make-color :red 0.0 :green 0.0 :blue 0.0)
-                    :background (xlib:make-color :red 1.0 :green 1.0 :blue 1.0)
-                    :source-font font
-                    :source-char code
-                    :mask-font font
-                    :mask-char (1+ code))))
+    (loop for (symbols code) in *clx-cursor-mapping*
+          for cursor = (xlib:create-glyph-cursor
+                        :foreground (xlib:make-color :red 0.0 :green 0.0 :blue 0.0)
+                        :background (xlib:make-color :red 1.0 :green 1.0 :blue 1.0)
+                        :source-font font
+                        :source-char code
+                        :mask-font font
+                        :mask-char (1+ code))
+          do (dolist (symbol symbols)
+               (setf (gethash symbol (clx-port-cursor-table port)) cursor)))
     (xlib:close-font font)))
