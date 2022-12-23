2c55e0c06ca910f866a62866abbed5c459d82847
Daniel Kochmański <daniel@turtleware.eu>
2022-12-23 14:37:47 +0100

    examples: stream-test: print both events and gestures in the frame
    Previously we've printed directly to the terminal. Now we create two panes - one
    as previously being echo-interactor-pane, and the other as the destination for
    output. Moreover we print both handled events for the sheet and read gestures
    for the stream - in red and green color accordingly.

--- Examples/stream-test.lisp
+++ Examples/stream-test.lisp
@@ -3,6 +3,7 @@
 ;;; ---------------------------------------------------------------------------
 ;;;
 ;;;  (c) Copyright 2001 by Tim Moore <moore@bricoworks.com>
+;;;  (c) Copyright 2022 by Daniel Kochmański <daniel@turtleware.eu>
 ;;;
 ;;; ---------------------------------------------------------------------------
 ;;;
@@ -17,13 +18,22 @@
   (let* ((results (multiple-value-list (call-next-method)))
          (gesture (car results)))
     (when gesture
-      (print gesture *trace-output*))
+      (with-drawing-options (t :ink +dark-green+)
+        (print gesture)))
     (values-list results)))
+
+(defmethod handle-event :around ((stream echo-interactor-pane) event)
+  (with-drawing-options (t :ink +dark-red+)
+    (print event))
+  (call-next-method))
 
 (define-application-frame stream-test ()
   ()
   (:menu-bar nil)
   (:panes
-   (tester (make-clim-stream-pane :type 'echo-interactor-pane)))
+   (tester (make-clim-stream-pane :type 'echo-interactor-pane))
+   (shower :application))
   (:layouts
-   (default (vertically () tester))))
+   (default (vertically ()
+              tester
+              shower))))
